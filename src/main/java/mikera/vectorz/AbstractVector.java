00d8b7a479a5b3a80d19c29f8d0fe865545d39b7
mikera <mike.r.anderson.13@gmail.com>
2012-09-28 11:51:32 +0800

    Separate out offset-based SubVector from 0-based Vector

--- src/main/java/mikera/vectorz/AbstractVector.java
+++ src/main/java/mikera/vectorz/AbstractVector.java
@@ -2,9 +2,33 @@ package mikera.vectorz;
 
 public abstract class AbstractVector {
 
+	// ================================================
+	// Abstract interface
 	public abstract int length();
 
 	public abstract double get(int i);
 	
 	public abstract void set(int i, double value);
+	
+	
+	
+	// ================================================
+	// Standard implementations
+	
+	@Override
+	public int hashCode() {
+		int hashCode = 1;
+		int len=length();
+		for (int i = 0; i < len; i++) {
+			hashCode = 31 * hashCode + (Tools.hashCode(get(i)));
+		}
+		return hashCode;
+	}
+
+	public void copyTo(double[] data, int offset) {
+		int len = length();
+		for (int i=0; i<len; i++) {
+			data[i+offset]=get(i);
+		}
+	}
 }
---
+++ src/main/java/mikera/vectorz/ArrayVector.java
@@ -0,0 +1,9 @@
+package mikera.vectorz;
+
+public abstract class ArrayVector extends AbstractVector {
+
+	public abstract double[] getArray();
+	
+	public abstract int getArrayOffset();
+
+}
