16b47ecc410333073af5e9f4ffea0b1a0690c29c
mikera <Mike@Mike-VAIO>
2012-09-28 11:51:32 +0800

    Separate out offset-based SubVector from 0-based Vector

---
+++ src/main/java/mikera/vectorz/SubVector.java
@@ -0,0 +1,165 @@
+package mikera.vectorz;
+
+import mikera.vectorz.Tools;
+
+/**
+ * Class representing a fixed-size vector of mutable double values
+ * 
+ * @author Mike
+ * 
+ */
+public final class SubVector extends ArrayVector {
+	private final double[] data;
+
+	private final int offset;
+	private final int length;
+
+	public SubVector(double[] values) {
+		offset = 0;
+		length = values.length;
+		data = new double[length];
+		System.arraycopy(values, 0, data, 0, length);
+	}
+
+	public SubVector(int length) {
+		this.length = length;
+		offset = 0;
+		data = new double[length];
+	}
+
+	public SubVector(SubVector source) {
+		length = source.length;
+		this.offset = 0;
+		data = new double[length];
+		System.arraycopy(source.data, source.offset, this.data, 0, length);
+	}
+
+	/**
+	 * Constructs a vector directly referencing a sub-vector of an existing
+	 * array-based Vector
+	 * 
+	 * @param source
+	 * @param offset
+	 * @param length
+	 */
+	SubVector(ArrayVector source, int offset, int length) {
+		int len=source.length();
+		if (offset < 0) {
+			throw new IndexOutOfBoundsException("Negative offset for Vector: "
+					+ offset);
+		}
+		if (offset + length > len) {
+			throw new IndexOutOfBoundsException(
+					"Beyond bounds of parent vector with offset: " + offset
+							+ " and length: " + length);
+		}
+		this.length = length;
+		this.offset = source.getArrayOffset() + offset;
+		this.data = source.getArray();
+	}
+
+	@Override
+	public int length() {
+		return length;
+	}
+
+	/**
+	 * Returns a vector referencing a sub-vector of the current vector
+	 * 
+	 * @param offset
+	 * @param length
+	 * @return
+	 */
+	public SubVector subVector(int offset, int length) {
+		if ((offset + length) > this.length)
+			throw new IndexOutOfBoundsException("Upper bound " + this.length
+					+ " breached:" + (offset + length));
+		if (offset < 0)
+			throw new IndexOutOfBoundsException("Lower bound breached:"
+					+ offset);
+		return new SubVector(this, offset, length);
+	}
+
+	@Override
+	public double get(int i) {
+		if ((i < 0) || (i > length))
+			throw new IndexOutOfBoundsException("Index = " + i);
+		return data[offset + i];
+	}
+
+	@Override
+	public void set(int i, double value) {
+		if ((i < 0) || (i > length))
+			throw new IndexOutOfBoundsException("Index = " + i);
+		data[offset + i] = value;
+	}
+
+	/**
+	 * Test for equality on vectors. Returns true iff all values in the vector
+	 * are identical
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof SubVector))
+			return false;
+		SubVector v = (SubVector) o;
+		if (length != v.length)
+			return false;
+		if ((data == v.data) && (offset == v.offset))
+			return true;
+		for (int i = 0; i < length; i++) {
+			if (v.data[v.offset + i] != data[offset + i])
+				return false;
+		}
+		return true;
+	}
+
+	public void add(SubVector v) {
+		if (v.length != length) {
+			throw new Error("Source vector has different size: " + v.length);
+		}
+		for (int i = 0; i < length; i++) {
+			data[offset + i] += v.data[v.offset + i];
+		}
+	}
+	
+	public void addMultiple(SubVector v, double factor) {
+		if (v.length != length) {
+			throw new Error("Source vector has different size: " + v.length);
+		}
+		for (int i = 0; i < length; i++) {
+			data[offset + i] += v.data[v.offset + i]*factor;
+		}
+	}
+	
+	public void fill(double value) {
+		for (int i = 0; i < length; i++) {
+			data[offset + i] += value;
+		}
+	}
+
+
+	/**
+	 * Vector hashcode, designed to match hashcode of Java double array
+	 */
+	@Override
+	public int hashCode() {
+		int hashCode = 1;
+		for (int i = 0; i < length; i++) {
+			hashCode = 31 * hashCode + (Tools.hashCode(data[offset+i]));
+		}
+		return hashCode;
+	}
+
+	@Override
+	public double[] getArray() {
+		return data;
+	}
+
+	@Override
+	public int getArrayOffset() {
+		return offset;
+	}
+}
--- src/test/java/mikera/vectorz/TestVector.java
+++ src/test/java/mikera/vectorz/TestVector.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
-import mikera.vectorz.Vector;
+import mikera.vectorz.SubVector;
 
 import org.junit.Test;
 
@@ -14,14 +14,14 @@ public class TestVector {
 		double[] data=new double[100];
 		for (int i=0; i<100; i++) data[i]=i;
 		
-		Vector v=new Vector(data);
+		SubVector v=new SubVector(data);
 		assertEquals(10,v.get(10),0.0);
 		
-		Vector v2=v.subVector(5, 90);
+		SubVector v2=v.subVector(5, 90);
 		assertEquals(90,v2.length());
 		assertEquals(15,v2.get(10),0.0);
 		
-		Vector v3=v2.subVector(5,80);
+		SubVector v3=v2.subVector(5,80);
 		assertEquals(20,v3.get(10),0.0);
 		
 		v3.set(10, -99);
@@ -31,11 +31,11 @@ public class TestVector {
 	@Test public void testClone() {
 		double[] data=new double[100];
 		for (int i=0; i<100; i++) data[i]=i;
-		Vector v=new Vector(data);	
+		SubVector v=new SubVector(data);	
 		
 		assertEquals(Arrays.hashCode(data),v.hashCode());
 		
-		Vector v2=new Vector(v);
+		SubVector v2=new SubVector(v);
 		assertEquals(v,v2);
 		
 		v.set(10,Math.PI);
