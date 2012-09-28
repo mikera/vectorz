dee9e49cff40ebaca80690277addf266e61c88a1
mikera <mike.r.anderson.13@gmail.com>
2012-09-28 15:32:48 +0800

    Magnitude tests

--- src/test/java/mikera/vectorz/TestVectorMath.java
+++ src/test/java/mikera/vectorz/TestVectorMath.java
@@ -9,4 +9,9 @@ public class TestVectorMath {
 	@Test public void testDotProduct() {
 		assertEquals(10.0,new Vector3(1,2,3).dotProduct(new Vector3(3,2,1)),0.000001);
 	}
+	
+	@Test public void testMagnitude() {
+		assertEquals(14.0,new Vector3(1,-2,3).magnitudeSquared(),0.000001);
+		assertEquals(5.0,new Vector2(3,4).magnitude(),0.000001);
+	}
 }
