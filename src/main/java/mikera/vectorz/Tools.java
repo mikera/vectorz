84f687fc410ed0efd32affaba1159cc254db1266
mikera <mike.r.anderson.13@gmail.com>
2012-09-28 11:29:03 +0800

    Initial code checkin

---
+++ src/main/java/mikera/vectorz/Tools.java
@@ -0,0 +1,39 @@
+package mikera.vectorz;
+
+public final class Tools {
+	public static void debugBreak(Object o) {
+		o.toString();
+	}
+	
+	/**
+	 * Hashcode for an int, defined as the value of the int itself for consistency with java.lang.Integer
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static int hashCode(int value) {
+		return value;
+	}
+	
+	
+	/** 
+	 * Hashcode for a double primitive
+	 * 
+	 * @param d
+	 * @return
+	 */
+	public static int hashCode(double d) {
+		return hashCode(Double.doubleToLongBits(d));
+	}
+	
+	/**
+	 * Hashcode for a long primitive
+	 * @param l
+	 * @return
+	 */
+	public static int hashCode(long l) {
+		return (int) (l ^ (l >>> 32));
+	}
+
+	
+}
---
+++ src/main/java/mikera/vectorz/Vector.java
@@ -0,0 +1,151 @@
+package mikera.vectorz;
+
+import mikera.vectorz.Tools;
+
+/**
+ * Class representing a fixed-size vector of mutable double values
+ * 
+ * @author Mike
+ * 
+ */
+public final class Vector {
+	private final double[] data;
+
+	private final int offset;
+	private final int length;
+
+	public Vector(double[] values) {
+		offset = 0;
+		length = values.length;
+		data = new double[length];
+		System.arraycopy(values, 0, data, 0, length);
+	}
+
+	public Vector(int length) {
+		this.length = length;
+		offset = 0;
+		data = new double[length];
+	}
+
+	public Vector(Vector source) {
+		length = source.length;
+		this.offset = 0;
+		data = new double[length];
+		System.arraycopy(source.data, source.offset, this.data, 0, length);
+	}
+
+	/**
+	 * Constructs a vector directly referencing a sub-vector of an existing
+	 * Vector
+	 * 
+	 * @param source
+	 * @param offset
+	 * @param length
+	 */
+	private Vector(Vector source, int offset, int length) {
+		if (offset < 0) {
+			throw new IndexOutOfBoundsException("Negative offset for Vector: "
+					+ offset);
+		}
+		if (offset + length > source.length) {
+			throw new IndexOutOfBoundsException(
+					"Beyond bounds of parent vector with offset: " + offset
+							+ " and length: " + length);
+		}
+		this.length = length;
+		this.offset = source.offset + offset;
+		this.data = source.data;
+	}
+
+	public int length() {
+		return length;
+	}
+
+	/**
+	 * Returns a vector referencing a sub-vector of the current vector
+	 * 
+	 * @param offset
+	 * @param length
+	 * @return
+	 */
+	public Vector subVector(int offset, int length) {
+		if ((offset + length) > this.length)
+			throw new IndexOutOfBoundsException("Upper bound " + this.length
+					+ " breached:" + (offset + length));
+		if (offset < 0)
+			throw new IndexOutOfBoundsException("Lower bound breached:"
+					+ offset);
+		return new Vector(this, offset, length);
+	}
+
+	public double get(int i) {
+		if ((i < 0) || (i > length))
+			throw new IndexOutOfBoundsException("Index = " + i);
+		return data[offset + i];
+	}
+
+	public void set(int i, double value) {
+		if ((i < 0) || (i > length))
+			throw new IndexOutOfBoundsException("Index = " + i);
+		data[offset + i] = value;
+	}
+
+	/**
+	 * Test for equality on vectors. Returns true iff all values in the vector
+	 * are identical
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Vector))
+			return false;
+		Vector v = (Vector) o;
+		if (length != v.length)
+			return false;
+		if ((data == v.data) && (offset == v.offset))
+			return true;
+		for (int i = 0; i < length; i++) {
+			if (v.data[v.offset + i] != data[offset + i])
+				return false;
+		}
+		return true;
+	}
+
+	public void add(Vector v) {
+		if (v.length != length) {
+			throw new Error("Source vector has different size: " + v.length);
+		}
+		for (int i = 0; i < length; i++) {
+			data[offset + i] += v.data[v.offset + i];
+		}
+	}
+	
+	public void addMultiple(Vector v, double factor) {
+		if (v.length != length) {
+			throw new Error("Source vector has different size: " + v.length);
+		}
+		for (int i = 0; i < length; i++) {
+			data[offset + i] += v.data[v.offset + i]*factor;
+		}
+	}
+	
+	public void fill(double value) {
+		for (int i = 0; i < length; i++) {
+			data[offset + i] += value;
+		}
+	}
+
+
+	/**
+	 * Vector hashcode, designed to match hashcode of Java double array
+	 */
+	@Override
+	public int hashCode() {
+		int hashCode = 1;
+		for (int i = 0; i < length; i++) {
+			hashCode = 31 * hashCode + (Tools.hashCode(data[offset+i]));
+		}
+		return hashCode;
+	}
+}
