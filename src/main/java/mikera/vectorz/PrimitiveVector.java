10f42506a6165f72f943614e63dd15d6995e294e
mikera <mike.r.anderson.13@gmail.com>
2012-09-28 12:29:56 +0800

    Factor out common parent class for primitive vectors

---
+++ src/main/java/mikera/vectorz/PrimitiveVector.java
@@ -0,0 +1,5 @@
+package mikera.vectorz;
+
+public abstract class PrimitiveVector extends AVector {
+
+}
--- src/main/java/mikera/vectorz/Vector3.java
+++ src/main/java/mikera/vectorz/Vector3.java
@@ -1,6 +1,6 @@
 package mikera.vectorz;
 
-public class Vector3 extends AVector {
+public class Vector3 extends PrimitiveVector {
 	public double x;
 	public double y;
 	public double z;
