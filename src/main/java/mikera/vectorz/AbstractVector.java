1ecdda2a6807c3246a2f01fcb0b6b3126dacad4d
mikera <Mike@Mike-VAIO>
2012-09-28 11:33:34 +0800

    Factor out common vector features into AbstractVector

---
+++ src/main/java/mikera/vectorz/AbstractVector.java
@@ -0,0 +1,10 @@
+package mikera.vectorz;
+
+public abstract class AbstractVector {
+
+	public abstract int length();
+
+	public abstract double get(int i);
+	
+	public abstract void set(int i, double value);
+}
