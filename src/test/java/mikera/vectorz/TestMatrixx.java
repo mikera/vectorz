017110d5717628d43cf89a56195db95cb3655fde
mikera <mike.r.anderson.13@gmail.com>
2012-09-28 22:26:20 +0800

    Scaling matrix

--- src/test/java/mikera/vectorz/TestMatrixx.java
+++ src/test/java/mikera/vectorz/TestMatrixx.java
@@ -26,4 +26,23 @@ public class TestMatrixx {
 			assertTrue(v.approxEquals(tv));
 		}
 	}
+	
+
+	@Test
+	public void testScale() {
+		for (int i=1; i<10; i++) {
+			AVector v=Vectorz.createLength(i);
+			for (int j=0; j<v.length(); j++) {
+				v.set(j,j+1.3);
+			}
+			
+			AVector tv=v.clone();
+			
+			AMatrix m=Matrixx.createScaleMatrix(i,2.3);
+			
+			m.transform(v, tv);
+			
+			assertEquals(v.magnitude()*2.3,tv.magnitude(),0.0001);
+		}
+	}
 }
