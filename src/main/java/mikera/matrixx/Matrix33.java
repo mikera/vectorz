391a7e879c859f1caf756526b1817715297dbe01
mikera <mike.r.anderson.13@gmail.com>
2012-09-30 09:23:29 +0800

    Add specialised 3x3 matrix

---
+++ src/main/java/mikera/matrixx/Matrix33.java
@@ -0,0 +1,114 @@
+package mikera.matrixx;
+
+import mikera.vectorz.AVector;
+import mikera.vectorz.Vector3;
+
+/**
+ * Specialised 3*3 Matrix for Vector3 maths, using primitive matrix elements
+ * 
+ * @author Mike
+ *
+ */
+public final class Matrix33 extends AMatrix {
+	private double m00,m01,m02,
+	               m10,m11,m12,
+	               m20,m21,m22;
+	
+	public Matrix33() {
+	}
+
+	@Override
+	public int rowCount() {
+		return 3;
+	}
+
+	@Override
+	public int columnCount() {
+		return 3;
+	}
+
+	@Override
+	public double get(int row, int column) {
+		switch (row) {
+		case 0:
+			switch (column) {
+			case 0: return m00;
+			case 1: return m01;
+			case 2: return m02;
+			default: throw new IndexOutOfBoundsException("Column: "+row);
+			}
+		case 1:
+			switch (column) {
+			case 0: return m10;
+			case 1: return m11;
+			case 2: return m12;
+			default: throw new IndexOutOfBoundsException("Column: "+row);
+			}
+		case 2:
+			switch (column) {
+			case 0: return m20;
+			case 1: return m21;
+			case 2: return m22;
+			default: throw new IndexOutOfBoundsException("Column: "+row);
+			}
+
+		default: throw new IndexOutOfBoundsException("Row: "+row);
+		}
+	}
+
+	@Override
+	public void set(int row, int column, double value) {
+		switch (row) {
+		case 0:
+			switch (column) {
+			case 0: m00=value; return;
+			case 1: m01=value; return;
+			case 2: m02=value; return;
+			default: throw new IndexOutOfBoundsException("Column: "+row);
+			}
+		case 1:
+			switch (column) {
+			case 0: m10=value; return;
+			case 1: m11=value; return;
+			case 2: m12=value; return;
+			default: throw new IndexOutOfBoundsException("Column: "+row);
+			}
+		case 2:
+			switch (column) {
+			case 0: m20=value; return;
+			case 1: m21=value; return;
+			case 2: m22=value; return;
+			default: throw new IndexOutOfBoundsException("Column: "+row);
+			}
+
+		default: throw new IndexOutOfBoundsException("Row: "+row);
+		}	
+	}
+	
+	@Override
+	public void transform(AVector source, AVector dest) {
+		if (source instanceof Vector3) {transform((Vector3)source,dest); return;}
+		super.transform(source,dest);
+	}
+	
+	public void transform(Vector3 source, AVector dest) {
+		if (dest instanceof Vector3) {transform(source,(Vector3)dest); return;}
+		Vector3 s=source;
+		dest.set(0,(m00*s.x)+(m01*s.y)+(m02*s.z));
+		dest.set(1,(m10*s.x)+(m11*s.y)+(m12*s.z));
+		dest.set(2,(m20*s.x)+(m21*s.y)+(m22*s.z));
+	}
+	
+	public void transform(Vector3 source, Vector3 dest) {
+		Vector3 s=source;
+		dest.x=((m00*s.x)+(m01*s.y)+(m02*s.z));
+		dest.y=((m10*s.x)+(m11*s.y)+(m12*s.z));
+		dest.z=((m20*s.x)+(m21*s.y)+(m22*s.z));
+	}
+	
+	@Override
+	public boolean isSquare() {
+		return true;
+	}
+
+}
