995beca786f95a4d9fc5f337f6ef78eb47506e64
mikera <mike.r.anderson.13@gmail.com>
2012-09-30 09:11:59 +0800

    Add M*N matrix class

---
+++ src/main/java/mikera/matrixx/MatrixMN.java
@@ -0,0 +1,46 @@
+package mikera.matrixx;
+
+import mikera.vectorz.AVector;
+
+public final class MatrixMN extends AVectorMatrix {
+	private int rowCount;	
+	private int columnCount;	
+	private AVector[] rows;
+	
+	@Override
+	public AVector getRow(int row) {
+		return rows[row];
+	}
+
+	@Override
+	public int rowCount() {
+		return rowCount;
+	}
+
+	@Override
+	public int columnCount() {
+		return columnCount;
+	}
+	
+	@Override
+	public double get(int row, int column) {
+		return rows[row].get(column);
+	}
+
+	@Override
+	public void set(int row, int column, double value) {
+		rows[row].set(column,value);
+	}
+	
+	@Override
+	public void transform(AVector source, AVector dest) {
+		for (int i=0; i<rowCount; i++) {
+			dest.set(i,getRow(i).dotProduct(source));
+		}
+	}
+	
+	@Override
+	public boolean isSquare() {
+		return rowCount==columnCount;
+	}
+}
