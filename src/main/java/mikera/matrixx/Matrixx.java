da8fd23e8f68d83e1f455945a20c53b21ced1849
mikera <mike.r.anderson.13@gmail.com>
2012-09-30 15:30:42 +0800

    Random square matrix generator

--- src/main/java/mikera/matrixx/Matrixx.java
+++ src/main/java/mikera/matrixx/Matrixx.java
@@ -15,13 +15,28 @@ public class Matrixx {
 		return im;
 	}
 	
-	
-	
 	public static DiagonalMatrix createScaleMatrix(int dimensions, double factor) {
 		DiagonalMatrix im=new DiagonalMatrix(dimensions);
 		for (int i=0; i<dimensions; i++) {
 			im.set(i,i,factor);
 		}
 		return im;
+	}
+	
+	public static AMatrix createRandomSquareMatrix(int dimensions) {
+		AMatrix m=createSquareMatrix(dimensions);
+		for (int i=0; i<dimensions; i++) {
+			for (int j=0; j<dimensions; j++) {
+				m.set(i,j,Math.random());
+			}
+		}
+		return m;
+	}
+
+	private static AMatrix createSquareMatrix(int dimensions) {
+		switch (dimensions) {
+		case 3: return new Matrix33();
+		default: return new MatrixMN(dimensions,dimensions);
+		}
 	}
 }
