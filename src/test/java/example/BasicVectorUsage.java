228782157d3c5cf3924974de0ba8cf65b83d1fce
mikera <mike.r.anderson.13@gmail.com>
2012-10-08 21:13:19 +0800

    Update basic usage example

--- src/test/java/example/BasicVectorUsage.java
+++ src/test/java/example/BasicVectorUsage.java
@@ -1,5 +1,7 @@
 package example;
 
+import mikera.matrixx.Matrix33;
+import mikera.matrixx.Matrixx;
 import mikera.vectorz.Vector3;
 
 public class BasicVectorUsage {
@@ -17,6 +19,11 @@ public class BasicVectorUsage {
 		Vector3 d=Vector3.of(10.0,0.0,0.0);		
 		d.addMultiple(v, 5.0);
 		System.out.println(d);	
+		
+		Matrix33 m=Matrixx.createXAxisRotationMatrix(Math.PI);
+		Vector3 rotated=m.transform(d);      // rotate 180 degrees around x axis     
+		System.out.println(rotated);	
+
 	}
 
 }
