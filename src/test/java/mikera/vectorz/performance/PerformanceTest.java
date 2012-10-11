19b2608c39ea86497f3b1e9a02ca2de864d4d42b
mikera <mike.r.anderson.13@gmail.com>
2012-10-11 09:23:58 +0800

    Add matrix inverse benchmark

--- src/test/java/mikera/vectorz/performance/PerformanceTest.java
+++ src/test/java/mikera/vectorz/performance/PerformanceTest.java
@@ -3,6 +3,9 @@ package mikera.vectorz.performance;
 import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 
+import mikera.matrixx.AMatrix;
+import mikera.matrixx.Matrix33;
+import mikera.matrixx.Matrixx;
 import mikera.vectorz.AVector;
 import mikera.vectorz.Vector;
 import mikera.vectorz.Vector3;
@@ -23,12 +26,29 @@ public class PerformanceTest extends SimpleBenchmark {
 		}
 	}
 	
+	public void timeMatrix3Rotation(int runs) {
+		Vector3 axis=Vector3.of(1,2,3);
+		Vector3 v=Vector3.of(Math.random(),Math.random(),Math.random());
+
+		Matrix33 rot=Matrixx.createRotationMatrix(axis, Math.random());
+		for (int i=0; i<runs; i++) {
+			rot.transformInPlace(v);
+		}
+	}
+	
 	public void timeVectorAddMultiple(int runs) {
 		AVector v=Vector.of(1,2,3);
-		AVector v2=Vector3.of(1,2,3);
+		AVector v2=Vector.of(1,2,3);
 		
 		for (int i=0; i<runs; i++) {
 			v.addMultiple(v2,2.0);
+		}
+	}
+	
+	public void timeMatrixInverse(int runs) {
+		AMatrix m=Matrixx.createRandomSquareMatrix(5);
+		for (int i=0; i<runs; i++) {
+			m=m.inverse();
 		}
 	}
 	
@@ -40,7 +60,8 @@ public class PerformanceTest extends SimpleBenchmark {
 	}
 
 	private void run() {
-		new Runner().run(new String[] {this.getClass().getCanonicalName()});
+		Runner runner=new Runner();
+		runner.run(new String[] {this.getClass().getCanonicalName()});
 	}
 
 }
