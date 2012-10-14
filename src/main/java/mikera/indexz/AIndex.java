9d504e7f5be44ca3d648155f0ce0b1207dfe7ce0
mikera <mike.r.anderson.13@gmail.com>
2012-10-14 10:15:11 +0800

    Refactor index functionality into abstract base class where appropriate

--- src/main/java/mikera/indexz/AIndex.java
+++ src/main/java/mikera/indexz/AIndex.java
@@ -1,6 +1,9 @@
 package mikera.indexz;
 
 import java.io.Serializable;
+import java.util.HashSet;
+
+import mikera.vectorz.Tools;
 
 /**
  * Abstract base class for Index functionality
@@ -9,11 +12,115 @@ import java.io.Serializable;
  */
 @SuppressWarnings("serial")
 public abstract class AIndex  implements Serializable, Cloneable {
+	// ===================================
+	// Abstract interface
+	
 	public abstract int get(int i);
 
+	public abstract int length();
+	
+	// ===================================
+	// Common implementations
+	
 	public void set(int i, int value) {
 		throw new UnsupportedOperationException();
 	}
 	
-	public abstract int length();
+	public boolean isFullyMutable() {
+		return false;
+	}
+	
+	public void copyTo(int[] array, int offset) {
+		int len=length();
+		for (int i=0; i<len; i++) {
+			array[offset+i]=get(i);
+		}
+	}
+	
+	public int[] toArray() {
+		int len=length();
+		int[] arr=new int[len];
+		copyTo(arr,0);
+		return arr;
+	}
+	
+
+	public boolean isSorted() {
+		int len=length();
+		for (int i=1; i<len; i++) {
+			if (get(i-1)>get(i)) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Returns true if this Index contains only distinct integer indices
+	 * @return
+	 */
+	public boolean isDistinct() {
+		HashSet<Integer> hs=new HashSet<Integer>();
+		int len=length();
+		for (int i=0; i<len; i++) {
+			Integer v=get(i);
+			if (hs.contains(v)) return false;
+			hs.add(v);
+		}
+		return true;
+	}
+	
+	public boolean contains(int index) {
+		int len=length();
+		for (int i=0; i<len; i++) {
+			if (get(i)==index) return true;
+		}
+		return false;
+	}
+	
+	public boolean contains(Index inds) {
+		int len=inds.length();
+		for (int i=0; i<len; i++) {
+			if (!contains(inds.get(i))) return false;
+		}
+		return true;
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof AIndex) return equals((AIndex)o);
+		return false;
+	}
+	
+	public boolean equals(AIndex o) {
+		int len=length();
+		if (len!=o.length()) return false;
+		for (int i=0; i<len; i++) {
+			if (get(i)!=o.get(i)) return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		int hashCode = 1;
+		int len=length();
+		for (int i = 0; i < len; i++) {
+			hashCode = 31 * hashCode + (Tools.hashCode(get(i)));
+		}
+		return hashCode;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb=new StringBuilder();
+		int length=length();
+		sb.append('[');
+		if (length>0) {
+			sb.append(get(0));
+			for (int i = 1; i < length; i++) {
+				sb.append(',');
+				sb.append(get(i));
+			}
+		}
+		sb.append(']');
+		return sb.toString();
+	}
 }
--- src/main/java/mikera/indexz/Index.java
+++ src/main/java/mikera/indexz/Index.java
@@ -1,6 +1,5 @@
 package mikera.indexz;
 
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -14,6 +13,7 @@ import mikera.vectorz.Tools;
  */
 public final class Index extends AIndex {
 	private static final long serialVersionUID = 8698831088064498284L;
+
 	final int[] data;
 	
 	public Index(int length) {
@@ -66,15 +66,6 @@ public final class Index extends AIndex {
 		return new Index(data.clone());
 	}
 	
-	@Override
-	public boolean equals(Object o) {
-		if (o instanceof Index) {
-			return equals((Index)o);
-		} else {
-			return false;
-		}
-	}
-	
 	public void permute(Index permutationIndex) {
 		int len=length();
 		int[] temp=data.clone();
@@ -86,9 +77,11 @@ public final class Index extends AIndex {
 	public void sort() {
 		Arrays.sort(data);
 	}
-	
-	public int[] toArray() {
-		return data.clone();
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Index) return equals((Index)o);
+		return super.equals(o);
 	}
 	
 	public boolean equals(Index o) {
@@ -99,80 +92,25 @@ public final class Index extends AIndex {
 		}
 		return true;
 	}
-	
-	@Override
-	public int hashCode() {
-		int hashCode = 1;
-		int len=length();
-		for (int i = 0; i < len; i++) {
-			hashCode = 31 * hashCode + (Tools.hashCode(get(i)));
-		}
-		return hashCode;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder sb=new StringBuilder();
-		int length=length();
-		sb.append('[');
-		if (length>0) {
-			sb.append(get(0));
-			for (int i = 1; i < length; i++) {
-				sb.append(',');
-				sb.append(get(i));
-			}
-		}
-		sb.append(']');
-		return sb.toString();
-	}
 
 	public int[] getData() {
 		return data;
 	}
 	
-	public boolean isSorted() {
-		int len=length();
-		for (int i=1; i<len; i++) {
-			if (data[i-1]>data[i]) return false;
-		}
+	@Override
+	public int[] toArray() {
+		return getData().clone();
+	}
+	
+	@Override
+	public boolean isFullyMutable() {
 		return true;
 	}
 	
-	/**
-	 * Returns true if this Index contains only distinct integer indices
-	 * @return
-	 */
-	public boolean isDistinct() {
-		HashSet<Integer> hs=new HashSet<Integer>();
-		int len=length();
-		for (int i=0; i<len; i++) {
-			Integer v=i;
-			if (hs.contains(v)) return false;
-			hs.add(v);
-		}
-		return true;
-	}
-
 	public void lookupWith(Index source) {
 		int len=length();
 		for (int i=0; i<len; i++) {
 			data[i]=source.data[data[i]];
 		}
-	}
-
-	public boolean contains(int index) {
-		int len=length();
-		for (int i=0; i<len; i++) {
-			if (data[i]==index) return true;
-		}
-		return false;
-	}
-	
-	public boolean contains(Index inds) {
-		int len=inds.length();
-		for (int i=0; i<len; i++) {
-			if (!contains(inds.get(i))) return false;
-		}
-		return true;
 	}
 }
