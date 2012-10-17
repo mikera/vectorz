b9855908151f4bfa496a1709544c2023a48fbc91
mikera <mike.r.anderson.13@gmail.com>
2012-10-18 07:53:36 +0800

    Add SequentialIndex

---
+++ src/main/java/mikera/indexz/impl/SequentialIndex.java
@@ -0,0 +1,20 @@
+package mikera.indexz.impl;
+
+import mikera.indexz.ComputedIndex;
+
+public class SequentialIndex extends ComputedIndex {
+	private static final long serialVersionUID = 8586796655048075367L;
+
+	private final int start;
+	
+	public SequentialIndex(int start, int length) {
+		super(length);
+		this.start=start;
+	}
+	
+	@Override
+	public int get(int i) {
+		assert((i>=0)&&(i<length));
+		return start+i;
+	}
+}
