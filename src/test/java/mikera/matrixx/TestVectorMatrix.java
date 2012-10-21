b8db07b6708d04327cb3cbc85e5e4f52d4d553d5
mikera <mike.r.anderson.13@gmail.com>
2012-10-21 17:11:29 +0800

    Tests for VectorMatrixMN

--- src/test/java/mikera/matrixx/TestVectorMatrix.java
+++ src/test/java/mikera/matrixx/TestVectorMatrix.java
@@ -2,6 +2,7 @@ package mikera.matrixx;
 
 import static org.junit.Assert.*;
 import mikera.matrixx.impl.VectorMatrixM3;
+import mikera.matrixx.impl.VectorMatrixMN;
 import mikera.vectorz.Vector;
 import mikera.vectorz.Vector1;
 import mikera.vectorz.Vector3;
@@ -11,8 +12,28 @@ import org.junit.Test;
 
 public class TestVectorMatrix {
 
-	@Test public void testCreate() {
+	@Test public void testCreateM3() {
 		VectorMatrixM3 m=new VectorMatrixM3(0);
+		assertEquals(0,m.rowCount());
+		assertEquals(3,m.columnCount());
+		
+		AMatrix mt=m.getTranspose();
+		assertEquals(0,mt.columnCount());
+		
+		m.appendRow(Vector.of(1,0,0));
+		assertEquals(1,m.rowCount());
+		
+		// transpose should be a changing view
+		assertEquals(1,mt.columnCount());
+		
+		m.appendRow(Vector3.of(0,1,0));
+		m.appendRow(Vectorz.join(Vector.of(0,0),Vector1.of(1)));
+		
+		assertTrue(m.epsilonEquals(Matrixx.createIdentityMatrix(3)));
+	}
+	
+	@Test public void testCreateMN() {
+		VectorMatrixMN m=new VectorMatrixMN(0,3);
 		assertEquals(0,m.rowCount());
 		assertEquals(3,m.columnCount());
 		
