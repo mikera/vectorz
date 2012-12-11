3daf7568bdcc9e7ba33e66d599b52ec62a027dd4
mikera <mike.r.anderson.13@gmail.com>
2012-12-11 17:18:42 +0800

    Add ZeroMatrix

---
+++ src/main/java/mikera/matrixx/impl/ZeroMatrix.java
@@ -0,0 +1,51 @@
+package mikera.matrixx.impl;
+
+import mikera.matrixx.AMatrix;
+
+/**
+ * Immutable zero matrix class
+ */
+public class ZeroMatrix extends AMatrix {
+	private final int inputDimensions;
+	private final int outputDimensions;
+
+	@Override public 
+	boolean isFullyMutable() {
+		return false;
+	}
+	
+	public ZeroMatrix(int rows, int columns) {
+		outputDimensions=rows;
+		inputDimensions=columns;
+	}
+	
+	@Override
+	public int inputDimensions() {
+		return inputDimensions;
+	}
+
+	@Override
+	public int outputDimensions() {
+		return outputDimensions;
+	}
+
+	@Override
+	public int rowCount() {
+		return inputDimensions;
+	}
+
+	@Override
+	public int columnCount() {
+		return outputDimensions;
+	}
+
+	@Override
+	public double get(int row, int column) {
+		return 0;
+	}
+
+	@Override
+	public void set(int row, int column, double value) {
+		throw new UnsupportedOperationException("ZeroMatrix is immutable!");
+	}
+}
