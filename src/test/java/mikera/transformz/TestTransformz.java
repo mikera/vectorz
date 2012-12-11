08bfdfc04cc180a0c2bf0ef19afae2552e51eafd
mikera <mike.r.anderson.13@gmail.com>
2012-12-11 17:36:54 +0800

    Basic transform tests

--- src/test/java/mikera/transformz/TestTransformz.java
+++ src/test/java/mikera/transformz/TestTransformz.java
@@ -1,9 +1,12 @@
 package mikera.transformz;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+import mikera.vectorz.AVector;
 import mikera.vectorz.Vector;
 import mikera.vectorz.Vector3;
+import mikera.vectorz.Vectorz;
 
 public class TestTransformz {
 
@@ -14,8 +17,24 @@ public class TestTransformz {
 		doTransformTests(Transformz.constantTransform(2,Vector.of(1,2,3,4,5)));
 
 	}
+	
+	public static void doApplyTest(ATransform t) {
+		AVector x=Vectorz.createUniformRandomVector(t.inputDimensions());
+		AVector y=Vectorz.newVector(t.outputDimensions());
+		y.fill(Double.NaN);
+		
+		AVector x2=x.clone();
+		t.transform(x2, y);
+		
+		// should overwrite everything
+		for (int i=0; i<y.length(); i++) {
+			assertNotEquals(Double.NaN,y.get(i));
+		}
+		
+		assertTrue(x.equals(x2));
+	}
 
 	public static void doTransformTests(ATransform t) {
-		
+		doApplyTest(t);
 	}
 }
