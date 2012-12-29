bcde00f61a988136a77bce8cfa00a0b084f1642c
mikera <mike.r.anderson.13@gmail.com>
2012-12-29 08:38:34 +0800

    ScalarMatrix transform

--- src/main/java/mikera/matrixx/impl/ScalarMatrix.java
+++ src/main/java/mikera/matrixx/impl/ScalarMatrix.java
@@ -1,6 +1,7 @@
 package mikera.matrixx.impl;
 
 import mikera.matrixx.AMatrix;
+import mikera.vectorz.AVector;
 
 /**
  * Scalar matrix class - i.e. multiplies every component by a constant factor
@@ -22,6 +23,13 @@ public class ScalarMatrix extends ADiagonalMatrix {
 
 	public static AMatrix create(int dimensions, double scale) {
 		return new ScalarMatrix(dimensions, scale);
+	}
+	
+	@Override
+	public void transformInPlace(AVector v) {
+		for (int i=0; i<dimensions; i++) {
+			v.scale(scale);
+		}
 	}
 
 
