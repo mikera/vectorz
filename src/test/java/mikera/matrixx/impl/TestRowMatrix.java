15614480814483cb55cccb9b6bf835cfc3b932f2
mikera <mike.r.anderson.13@gmail.com>
2012-12-31 10:31:06 +0800

    Test row matrix

--- src/test/java/mikera/matrixx/impl/TestRowMatrix.java
+++ src/test/java/mikera/matrixx/impl/TestRowMatrix.java
@@ -1,5 +1,6 @@
 package mikera.matrixx.impl;
 
+import static org.junit.Assert.*;
 import mikera.vectorz.Vector;
 
 import org.junit.Test;
@@ -7,5 +8,7 @@ import org.junit.Test;
 public class TestRowMatrix {
 	@Test public void testRM() {
 		RowMatrix rm=new RowMatrix(Vector.of(1,2,3,4));
+		
+		assertEquals(4,rm.columnCount());
 	}
 }
