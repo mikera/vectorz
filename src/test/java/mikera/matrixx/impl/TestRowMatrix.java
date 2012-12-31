c18d4b7b6e51670167d80c7285d7cbcb3ac1627c
mikera <mike.r.anderson.13@gmail.com>
2012-12-31 10:29:28 +0800

    Row matrix tests

---
+++ src/test/java/mikera/matrixx/impl/TestRowMatrix.java
@@ -0,0 +1,11 @@
+package mikera.matrixx.impl;
+
+import mikera.vectorz.Vector;
+
+import org.junit.Test;
+
+public class TestRowMatrix {
+	@Test public void testRM() {
+		RowMatrix rm=new RowMatrix(Vector.of(1,2,3,4));
+	}
+}
