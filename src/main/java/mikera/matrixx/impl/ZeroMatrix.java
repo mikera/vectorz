e12518a01710cf0d9e951f21b9053f2f6858f0a4
mikera <mike.r.anderson.13@gmail.com>
2013-01-06 11:29:09 +0800

    scale impl

--- src/main/java/mikera/matrixx/impl/ZeroMatrix.java
+++ src/main/java/mikera/matrixx/impl/ZeroMatrix.java
@@ -27,6 +27,11 @@ public final class ZeroMatrix extends AMatrix {
 	public int inputDimensions() {
 		return inputDimensions;
 	}
+	
+	@Override
+	public void scale(double factor) {
+		// no change
+	}
 
 	@Override
 	public int outputDimensions() {
