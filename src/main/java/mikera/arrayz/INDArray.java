64949fd24f5fd72ba4ebf499f4b80f66608b20d7
mikera <mike.r.anderson.13@gmail.com>
2013-01-07 00:33:03 +0800

    INDArray edits

--- src/main/java/mikera/arrayz/INDArray.java
+++ src/main/java/mikera/arrayz/INDArray.java
@@ -1,5 +1,12 @@
 package mikera.arrayz;
 
+/**
+ * Interface for general multi-dimensional arrays
+ * @author Mike
+ */
 public interface INDArray {
+	
+	public int dimensionality();
+	
 	public double get(int... indexes);
 }
