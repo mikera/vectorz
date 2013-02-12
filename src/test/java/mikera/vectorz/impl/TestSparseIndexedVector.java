f6cb6c120693535fcfe645e7d56c36de5ecf4158
mikera <mike.r.anderson.13@gmail.com>
2013-02-12 15:32:34 +0800

    TestSparseIndexedVector suite

---
+++ src/test/java/mikera/vectorz/impl/TestSparseIndexedVector.java
@@ -0,0 +1,20 @@
+package mikera.vectorz.impl;
+
+import static org.junit.Assert.*;
+import mikera.indexz.Index;
+import mikera.vectorz.Vector;
+
+import org.junit.Test;
+
+public class TestSparseIndexedVector {
+
+	@Test public void testConstruction() {
+		SparseIndexedVector sv=SparseIndexedVector.create(10, Index.of(1,3,6), Vector.of(1.0,2.0,3.0));
+		assertEquals(10,sv.length());
+		assertEquals(3,sv.nonSparseValues().length());
+		assertEquals(1.0,sv.get(1),0.0);
+		assertEquals(0.0,sv.get(9),0.0);
+		assertEquals(6.0,sv.elementSum(),0.0);
+
+	}
+}
