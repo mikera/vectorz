70e77cb7df498d4e4b3fc85eaa1e658353f9ef50
mikera <mike.r.anderson.13@gmail.com>
2013-02-12 14:44:33 +0800

    Add sparse vector base class

---
+++ src/main/java/mikera/vectorz/impl/ASparseVector.java
@@ -0,0 +1,18 @@
+package mikera.vectorz.impl;
+
+import mikera.vectorz.AVector;
+
+/**
+ * Abstract base class for Sparse vector implementations
+ * @author Mike
+ *
+ */
+public abstract class ASparseVector extends AVector {
+	private static final long serialVersionUID = -6043956533730989975L;
+
+	/**
+	 * Returns the number of non-sparse elements in the sparse vector.
+	 * @return
+	 */
+	public abstract int nonSparseElementCount();
+}
