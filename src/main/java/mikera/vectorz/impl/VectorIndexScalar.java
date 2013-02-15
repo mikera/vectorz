dc7f760dcb938fb2457794b47c1dc2afa96265b2
mikera <mike.r.anderson.13@gmail.com>
2013-02-15 12:22:29 +0800

    Add vectorindexscalar

---
+++ src/main/java/mikera/vectorz/impl/VectorIndexScalar.java
@@ -0,0 +1,26 @@
+package mikera.vectorz.impl;
+
+import mikera.vectorz.AScalar;
+import mikera.vectorz.AVector;
+
+public class VectorIndexScalar extends AScalar {
+	final AVector vector;
+	final int index;
+	
+	public VectorIndexScalar(AVector vector, int index) {
+		assert((index>=0)&&(index<vector.length()));
+		this.vector=vector;
+		this.index=index;
+	}
+	
+	@Override
+	public double get() {
+		return vector.get(index);
+	}
+	
+	@Override
+	public void set(double value) {
+		vector.set(index,value);
+	}
+
+}
