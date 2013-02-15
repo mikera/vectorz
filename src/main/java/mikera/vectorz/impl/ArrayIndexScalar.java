6ac09e7c51e40da992301803e6b8061a48ea141e
mikera <mike.r.anderson.13@gmail.com>
2013-02-15 12:14:03 +0800

    Add ArrayIndexScalar

---
+++ src/main/java/mikera/vectorz/impl/ArrayIndexScalar.java
@@ -0,0 +1,24 @@
+package mikera.vectorz.impl;
+
+import mikera.vectorz.AScalar;
+
+public class ArrayIndexScalar extends AScalar {
+	final double[] array;
+	final int index;
+	
+	public ArrayIndexScalar(double[] array, int index) {
+		this.array=array;
+		this.index=index;
+	}
+	
+	@Override
+	public double get() {
+		return array[index];
+	}
+	
+	@Override
+	public void set(double value) {
+		array[index]=value;
+	}
+
+}
