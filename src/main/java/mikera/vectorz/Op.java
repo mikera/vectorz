d05a957f9dca83fc502b560ef90e73ae6c4a7861
mikera <mike.r.anderson.13@gmail.com>
2013-02-20 19:57:06 +0800

    Add abstract apply function

--- src/main/java/mikera/vectorz/Op.java
+++ src/main/java/mikera/vectorz/Op.java
@@ -2,8 +2,15 @@ package mikera.vectorz;
 
 import mikera.transformz.impl.AOpTransform;
 
+/**
+ * Abstract class for representing a unary operation
+ * 
+ * @author Mike
+ */
 public abstract class Op implements IOp {
 
+	public abstract double apply(double x);
+	
 	@Override
 	public void applyTo(AVector v) {
 		if (v instanceof ArrayVector) {
@@ -31,12 +38,81 @@ public abstract class Op implements IOp {
 		return new AOpTransform(this,dims);
 	}
 	
+	/**
+	 * Returns the derivative of this Op for a given output value y
+	 * 
+	 * i.e. f'(g(y)) where f is the operator, g is the inverse of f 
+	 * 
+	 * @param y
+	 * @return
+	 */
+	public double derivativeForOutput(double y) {
+		throw new UnsupportedOperationException("No derivative defined for "+this.toString());
+	}
+	
+	/**
+	 * Returns the derivative of this Op for a given input value x
+	 * 
+	 * i.e. f'(x) where f is the operator
+	 * 
+	 * @param y
+	 * @return
+	 */
+	public double derivative(double x) {
+		return derivativeForOutput(apply(x));
+	}
+	
+	public boolean isStochastic() {
+		return false;
+	}
+	
 	public double minValue() {
 		return -Double.MAX_VALUE;
 	}
 	
 	public double maxValue() {
 		return Double.MAX_VALUE;
+	}
+	
+	/**
+	 * Validates whether all values in a double[] are within the possible output range for this Op
+	 * @param output
+	 * @return
+	 */
+	public boolean validateOutput(double[] output) {
+		double min=minValue();
+		double max=maxValue();
+		for (double d: output) {
+			if ((d<min)||(d>max)) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Creates a copy of the values of src in dest, constraining them to be within the valid
+	 * range of output values from this Op
+	 * @param src
+	 * @param dest
+	 * @param offset
+	 * @param length
+	 */
+	public void constrainValues(double[] src, double[] dest, int offset, int length) {
+		if (!isBounded()) {
+			System.arraycopy(src, 0, dest, offset, length);
+		}
+		double min=minValue();
+		double max=maxValue();
+		
+		for (int i=offset; i<(offset+length); i++) {
+			double v=src[i];
+			if (v>max) {
+				dest[i]=max;
+			} else if (v<min) {
+				dest[i]=min;
+			} else {
+				dest[i]=v;
+			}
+		}		
 	}
 	
 	public boolean isBounded() {
