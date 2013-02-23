52750dc20e99c4f5fbccc58dc0b8dda59dc31954
mikera <mike.r.anderson.13@gmail.com>
2013-02-23 14:44:10 +0800

    more tests

--- src/test/java/mikera/vectorz/impl/TestSparseIndexedVector.java
+++ src/test/java/mikera/vectorz/impl/TestSparseIndexedVector.java
@@ -2,7 +2,10 @@ package mikera.vectorz.impl;
 
 import static org.junit.Assert.*;
 import mikera.indexz.Index;
+import mikera.vectorz.AVector;
+import mikera.vectorz.ArrayVector;
 import mikera.vectorz.Vector;
+import mikera.vectorz.Vectorz;
 
 import org.junit.Test;
 
@@ -22,5 +25,29 @@ public class TestSparseIndexedVector {
 	@Test (expected=java.lang.Throwable.class)
 	public void testFaultyConstruction() {
 		SparseIndexedVector.create(10, Index.of(10,3,6), Vector.of(1.0,2.0,3.0));
+	}
+	
+	@Test public void testAddProduct() {
+		SparseIndexedVector sv=SparseIndexedVector.create(10, Index.of(1,3,6), Vector.of(1.0,2.0,3.0));
+		
+		AVector vz=Vectorz.newVector(10);
+		AVector vs=Vector.of(0,1,2,3,4,5,6,7,8,9);
+		
+		assertTrue(vz instanceof ArrayVector);
+		
+		AVector v=vz.exactClone();
+		v.addProduct(sv, vs);
+		assertEquals(Vector.of(0,1,0,6,0,0,18,0,0,0),v);
+		
+		v.addProduct(sv, vs,2.0);
+		assertEquals(Vector.of(0,3,0,18,0,0,54,0,0,0),v);
+
+		AVector v2=Vectorz.newVector(20).subVector(5, 10);
+		v2.addProduct(sv, vs);
+		assertEquals(Vector.of(0,1,0,6,0,0,18,0,0,0),v2);
+		
+		v2.addProduct(sv, vs,2.0);
+		assertEquals(Vector.of(0,3,0,18,0,0,54,0,0,0),v2);
+
 	}
 }
