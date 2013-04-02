ff064392521a00c713e9ef754b79553d1941e21a
mikera <mike.r.anderson.13@gmail.com>
2013-04-02 12:44:38 +0800

    Use addAt rather than get + set

--- src/main/java/mikera/vectorz/AVector.java
+++ src/main/java/mikera/vectorz/AVector.java
@@ -629,8 +629,7 @@ public abstract class AVector implements IVector, Cloneable, Comparable<AVector>
 			throw new IllegalArgumentException("Source vector has different size: " + vlength);
 		}
 		for (int i = 0; i < length; i++) {
-			double x=get(i)+v.get(i);
-			set(i,x);
+			addAt(i,v.get(i));
 		}
 	}
 	
@@ -643,8 +642,7 @@ public abstract class AVector implements IVector, Cloneable, Comparable<AVector>
 		assert(srcOffset>=0);
 		assert(srcOffset+length<=src.length());
 		for (int i = 0; i < length; i++) {
-			double x=get(i)+src.get(srcOffset+i);
-			set(i,x);
+			addAt(i,src.get(srcOffset+i));
 		}
 	}
 	
@@ -676,8 +674,7 @@ public abstract class AVector implements IVector, Cloneable, Comparable<AVector>
 		int length=length();
 		assert((a.length()==length)&&(b.length()==length));
 		for (int i = 0; i < length; i++) {
-			double x=get(i)+(a.get(i)*b.get(i)*factor);
-			set(i,x);
+			addAt(i,(a.get(i)*b.get(i)*factor));
 		}
 	}
 	
@@ -858,7 +855,7 @@ public abstract class AVector implements IVector, Cloneable, Comparable<AVector>
 	public void addProduct(AVector a, int aOffset, AVector b, int bOffset, double factor) {
 		int length=length();
 		for (int i=0; i<length; i++) {
-			set(i, get(i)+ (a.get(i+aOffset)* b.get(i+bOffset)*factor));
+			addAt(i, (a.get(i+aOffset)* b.get(i+bOffset)*factor));
 		}
 	}
 	
@@ -894,7 +891,7 @@ public abstract class AVector implements IVector, Cloneable, Comparable<AVector>
 	public void add(double constant) {
 		int len=length();
 		for (int i=0; i<len; i++) {
-			set(i,get(i)+constant);
+			addAt(i,constant);
 		}
 	}
 	
