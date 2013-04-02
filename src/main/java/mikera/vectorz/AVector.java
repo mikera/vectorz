45b78f17d08733bef74803b5e74fbba60d8d6ac6
mikera <mike.r.anderson.13@gmail.com>
2013-04-02 12:35:33 +0800

    addMultiple overrides

--- src/main/java/mikera/vectorz/AVector.java
+++ src/main/java/mikera/vectorz/AVector.java
@@ -794,7 +794,16 @@ public abstract class AVector implements IVector, Cloneable, Comparable<AVector>
 		assert(len==index.length());
 		for (int i=0; i<len; i++) {
 			int j=index.data[i];
-			this.set(j,this.get(j)+vector.array[i]*factor);
+			this.addAt(j,vector.array[i]*factor);
+		}
+	}
+	
+	public void addMultiple(AVector vector, Index index, double factor) {
+		int len=vector.length();
+		assert(len==index.length());
+		for (int i=0; i<len; i++) {
+			int j=index.data[i];
+			this.addAt(j,vector.get(i)*factor);
 		}
 	}
 
