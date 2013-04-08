f9fce276eb8180bbfb538a64f8a25e9b03343601
mikera <mike.r.anderson.13@gmail.com>
2013-04-08 12:26:32 +0800

    make methods static

--- src/main/java/mikera/arrayz/Arrayz.java
+++ src/main/java/mikera/arrayz/Arrayz.java
@@ -3,12 +3,12 @@ package mikera.arrayz;
 import mikera.vectorz.util.VectorzException;
 
 public class Arrayz {
-	public INDArray create(Object object) {
+	public static INDArray create(Object object) {
 		if (object instanceof INDArray) return ((INDArray)object).clone();
 		throw new VectorzException("Don't know how to create array from: "+object.getClass());
 	}
 	
-	public INDArray create(Object... os) {
+	public static INDArray create(Object... os) {
 		int n=os.length;
 		INDArray[] as=new INDArray[n];
 		for (int i=0; i<n; i++) {
