ea5d2f36e1e088d0f2b99e77f3ac73ec720309bd
mikera <mike.r.anderson.13@gmail.com>
2013-05-10 11:51:09 +0800

    leading diagonal enhancements

--- src/main/java/mikera/matrixx/impl/ZeroMatrix.java
+++ src/main/java/mikera/matrixx/impl/ZeroMatrix.java
@@ -5,6 +5,7 @@ import mikera.matrixx.Matrixx;
 import mikera.randomz.Hash;
 import mikera.transformz.ATransform;
 import mikera.vectorz.AVector;
+import mikera.vectorz.impl.RepeatedElementVector;
 import mikera.vectorz.impl.ZeroVector;
 
 /**
@@ -152,6 +153,12 @@ public final class ZeroMatrix extends AMatrix {
 
 	public static ZeroMatrix create(int rows, int columns) {
 		return new ZeroMatrix(rows,columns);
+	}
+	
+	@Override
+	public AVector getLeadingDiagonal() {
+		assert(inputDimensions==outputDimensions);
+		return RepeatedElementVector.create(inputDimensions, 0.0);
 	}
 	
 	@Override
