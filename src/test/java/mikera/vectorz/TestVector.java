891aa97676fc9e28db7ceb097765326642995c61
mikera <mike.r.anderson.13@gmail.com>
2013-05-10 10:06:13 +0800

    tests and minor fixes

--- src/test/java/mikera/vectorz/TestVector.java
+++ src/test/java/mikera/vectorz/TestVector.java
@@ -9,6 +9,8 @@ public class TestVector {
 		Vector v=Vector.createLength(10);
 		Vectorz.fillGaussian(v);
 		
-		assertEquals(v.getClass(),v.subVector(0,5).join(v.subVector(5, 5)).getClass());
+		ArrayVector sv1=v.subVector(0, 5);
+		ArrayVector sv2=v.subVector(5, 5);
+		assertEquals(v.getClass(),sv1.join(sv2).getClass());
 	}
 }
