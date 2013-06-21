32d358b2fce2f0ff2d44a45bdfce595617416927
mikera <mike.r.anderson.13@gmail.com>
2013-06-22 00:13:30 +0100

    docs

--- src/main/java/mikera/vectorz/IScalar.java
+++ src/main/java/mikera/vectorz/IScalar.java
@@ -3,13 +3,20 @@ package mikera.vectorz;
 import mikera.arrayz.INDArray;
 
 /**
- * Interface for Scalar array objects
+ * Interface for scalar array objects
  * 
  * @author Mike
  */
 public interface IScalar extends INDArray {
 
+	/**
+	 * Returns the double value of a scalar array
+	 * @return
+	 */
 	public double get();
 	
+	/**
+	 * Sets the double value of a scalar array
+	 */
 	public void set(double value);
 }
