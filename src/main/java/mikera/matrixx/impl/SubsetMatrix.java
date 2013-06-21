f559a5d6afba5415624d167a45cebad55620df28
mikera <mike.r.anderson.13@gmail.com>
2013-06-21 22:32:59 +0100

    more overrides

--- src/main/java/mikera/matrixx/impl/SubsetMatrix.java
+++ src/main/java/mikera/matrixx/impl/SubsetMatrix.java
@@ -48,6 +48,11 @@ public final class SubsetMatrix extends AMatrix implements ISparse {
 	public double elementSum() {
 		return rowCount();
 	}
+	
+	@Override
+	public long nonZeroCount() {
+		return rowCount();
+	}
 
 	@Override
 	public int rowCount() {
