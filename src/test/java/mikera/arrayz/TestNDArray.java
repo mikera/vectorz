bb06f754f1624e80d249b0046859f8acefde1ef1
mikera <mike.r.anderson.13@gmail.com>
2013-06-22 01:36:33 +0100

    extra NDArray testing

--- src/test/java/mikera/arrayz/TestNDArray.java
+++ src/test/java/mikera/arrayz/TestNDArray.java
@@ -7,6 +7,13 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class TestNDArray {
+	@Test public void testScalarArray() {
+		NDArray a=NDArray.newArray();
+		a.set(3.0);
+		assertEquals(3.0,a.get(),0.0);
+		
+	}
+	
 	@Test public void testOuterProduct() {
 		NDArray a=new NDArray(1,2,3);
 		
