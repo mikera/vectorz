63cedcdac2f012a7f1087d06ee31fffb6d77ee08
mikera <mike.r.anderson.13@gmail.com>
2013-06-22 01:42:23 +0100

    more NDArray tests

--- src/test/java/mikera/arrayz/TestNDArray.java
+++ src/test/java/mikera/arrayz/TestNDArray.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
+import mikera.vectorz.impl.DoubleScalar;
+
 import org.junit.Test;
 
 public class TestNDArray {
@@ -14,6 +16,13 @@ public class TestNDArray {
 		assertEquals(1,a.elementCount());
 		assertEquals(1,a.nonZeroCount());
 		assertEquals(3.0,a.elementSum(),0.0);
+		
+		DoubleScalar s=new DoubleScalar(1.0);
+		s.set(a);
+		assertEquals(3.0,s.get(),0.0);
+		s.set(2.0);
+		a.set(s);
+		assertEquals(2.0,a.get(),0.0);
 		
 	}
 	
