343db47a79cefdbdddf7e9aed628ba23a4178934
mikera <mike.r.anderson.13@gmail.com>
2013-07-08 23:26:18 +0100

    zeromatrix overrides

--- src/main/java/mikera/matrixx/impl/ZeroMatrix.java
+++ src/main/java/mikera/matrixx/impl/ZeroMatrix.java
@@ -36,6 +36,21 @@ public final class ZeroMatrix extends AMatrix {
 	}
 	
 	@Override
+	public boolean isSymmetric() {
+		return isSquare();
+	}
+	
+	@Override
+	public boolean isUpperTriangular() {
+		return true;
+	}
+	
+	@Override
+	public boolean isLowerTriangular() {
+		return true;
+	}
+	
+	@Override
 	public void multiply(double factor) {
 		// no change
 	}
