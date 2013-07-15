8fd784d3d7de4d59ac97e9acf3dee2ff445e6e16
mikera <mike.r.anderson.13@gmail.com>
2013-07-15 23:05:56 +0100

    scalar code tidying

--- src/main/java/mikera/vectorz/impl/ArrayIndexScalar.java
+++ src/main/java/mikera/vectorz/impl/ArrayIndexScalar.java
@@ -6,37 +6,36 @@ import mikera.vectorz.util.VectorzException;
 public class ArrayIndexScalar extends AScalar {
 	final double[] array;
 	final int index;
-	
+
 	public ArrayIndexScalar(double[] array, int index) {
-		this.array=array;
-		this.index=index;
+		this.array = array;
+		this.index = index;
 	}
-	
+
 	public static ArrayIndexScalar wrap(double[] array, int index) {
-		return new ArrayIndexScalar(array,index);
+		return new ArrayIndexScalar(array, index);
 	}
-	
+
 	@Override
 	public double get() {
 		return array[index];
 	}
-	
+
 	@Override
 	public void set(double value) {
-		array[index]=value;
+		array[index] = value;
 	}
-	
+
 	@Override
 	public boolean isView() {
 		return true;
 	}
-	
+
 	@Override
 	public boolean isFullyMutable() {
 		return true;
 	}
-	
-	
+
 	@Override
 	public DoubleScalar clone() {
 		return DoubleScalar.create(array[index]);
@@ -44,12 +43,14 @@ public class ArrayIndexScalar extends AScalar {
 
 	@Override
 	public ArrayIndexScalar exactClone() {
-		return new ArrayIndexScalar(array.clone(),index);
+		return new ArrayIndexScalar(array.clone(), index);
 	}
 
 	@Override
 	public void validate() {
-		if ((index<0)||(index>=array.length)) throw new VectorzException("Index out of bounds");
+		if ((index < 0) || (index >= array.length)) { 
+			throw new VectorzException("Index out of bounds"); 
+		}
 		super.validate();
 	}
 }
