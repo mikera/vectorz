587c5d413b540a386b6490f75eaee9c65e8c1627
mikera <mike.r.anderson.13@gmail.com>
2013-07-21 13:20:40 +0100

    AStridedMatrix getRow and getColumn

--- src/main/java/mikera/matrixx/impl/AStridedMatrix.java
+++ src/main/java/mikera/matrixx/impl/AStridedMatrix.java
@@ -1,5 +1,8 @@
 package mikera.matrixx.impl;
 
+import mikera.vectorz.AVector;
+import mikera.vectorz.Vectorz;
+
 public abstract class AStridedMatrix extends AArrayMatrix {
 
 	protected AStridedMatrix(double[] data, int rows, int cols) {
@@ -11,4 +14,14 @@ public abstract class AStridedMatrix extends AArrayMatrix {
 	public abstract int rowStride();
 	
 	public abstract int columnStride();	
+	
+	@Override
+	public AVector getRow(int i) {
+		return Vectorz.wrapStrided(data, getArrayOffset()+i*rowStride(), cols, columnStride());
+	}
+	
+	@Override
+	public AVector getColumn(int i) {
+		return Vectorz.wrapStrided(data, getArrayOffset()+i*columnStride(), rows, rowStride());
+	}
 }
