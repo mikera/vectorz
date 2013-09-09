0d99761be5f45ae4baf6e17a9bb78c9c40517bcc
mikera <mike.r.anderson.13@gmail.com>
2013-09-09 12:43:55 +0800

    Implement BandedMatrixRow

--- src/main/java/mikera/matrixx/impl/ABandedMatrix.java
+++ src/main/java/mikera/matrixx/impl/ABandedMatrix.java
@@ -26,4 +26,62 @@ public abstract class ABandedMatrix extends AMatrix {
 	public boolean isFullyMutable() {
 		return false;
 	}
+	
+	@Override
+	public AVector getRow(int row) {
+		return new BandedMatrixRow(row);
+	}
+	
+	private final class BandedMatrixRow extends AVector {
+		int row;
+		int length;
+		int lower;
+		int upper;
+		public BandedMatrixRow(int row) {
+			this.row=row;
+			this.length=columnCount();
+			this.lower=lowerBandwidthLimit();
+			this.upper=upperBandwidthLimit();
+		}
+
+		@Override
+		public int length() {
+			return length;
+		}
+
+		@Override
+		public double get(int i) {
+			if ((i<0)||(i>=length)) throw new IndexOutOfBoundsException("Index: "+i);
+			return unsafeGet(i);
+		}
+		
+		@Override
+		public double unsafeGet(int i) {
+			int b=i-row;
+			if ((b<lower)||(b>upper)) return 0;
+			return getBand(b).unsafeGet(Math.min(i, row));
+		}
+
+		@Override
+		public void set(int i, double value) {
+			if ((i<0)||(i>=length)) throw new IndexOutOfBoundsException("Index: "+i);
+			unsafeSet(i,value);
+		}
+		
+		@Override
+		public void unsafeSet(int i, double value) {
+			int b=i-row;
+			getBand(b).unsafeSet(Math.min(i, row),value);
+		}
+
+		@Override
+		public AVector exactClone() {
+			return ABandedMatrix.this.exactClone().getRow(row);
+		}
+	
+		@Override
+		public boolean isFullyMutable() {
+			return (row<=-upper)&&((row+upper)>=length);
+		}
+	}
 }
