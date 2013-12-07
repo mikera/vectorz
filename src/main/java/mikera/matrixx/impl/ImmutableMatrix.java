e9e1d3c702125dc9133d2586b31dbdb59d7a67d2
mikera <mike.r.anderson.13@gmail.com>
2013-12-07 08:32:23 -0800

    more ImmutableMatrix overrides

--- src/main/java/mikera/matrixx/impl/ImmutableMatrix.java
+++ src/main/java/mikera/matrixx/impl/ImmutableMatrix.java
@@ -1,7 +1,10 @@
 package mikera.matrixx.impl;
 
+import java.nio.DoubleBuffer;
+
 import mikera.matrixx.AMatrix;
 import mikera.matrixx.Matrix;
+import mikera.vectorz.AVector;
 import mikera.vectorz.Vector;
 import mikera.vectorz.impl.ImmutableVector;
 import mikera.vectorz.util.DoubleArrays;
@@ -44,6 +47,11 @@ public final class ImmutableMatrix extends AMatrix {
 	}
 	
 	@Override
+	public long nonZeroCount() {
+		return DoubleArrays.nonZeroCount(data);
+	}
+	
+	@Override
 	public boolean isBoolean() {
 		return DoubleArrays.isBoolean(data,0,data.length);
 	}
@@ -79,11 +87,48 @@ public final class ImmutableMatrix extends AMatrix {
 	}
 	
 	@Override
+	public Vector innerProduct(AVector a) {
+		if (a instanceof Vector) return innerProduct((Vector)a);
+		return transform(a);
+	}
+	
+	@Override
+	public Vector transform (AVector a) {
+		Vector v=Vector.createLength(rows);
+		for (int i=0; i<rows; i++) {
+			v.data[i]=a.dotProduct(data, i*cols);
+		}
+		return v;
+	}
+	
+	@Override
+	public void transform(Vector source, Vector dest) {
+		int rc = rowCount();
+		int cc = columnCount();
+		if (source.length()!=cc) throw new IllegalArgumentException(ErrorMessages.wrongSourceLength(source));
+		if (dest.length()!=rc) throw new IllegalArgumentException(ErrorMessages.wrongDestLength(dest));
+		int di=0;
+		for (int row = 0; row < rc; row++) {
+			double total = 0.0;
+			for (int column = 0; column < cc; column++) {
+				total += data[di+column] * source.data[column];
+			}
+			di+=cc;
+			dest.data[row]=total;
+		}
+	}
+	
+	@Override
 	public final void copyColumnTo(int col, double[] dest, int destOffset) {
 		int colOffset=col;
 		for (int i=0;i<rows; i++) {
 			dest[destOffset+i]=data[colOffset+i*cols];
 		}
+	}
+	
+	@Override
+	public double elementSum() {
+		return DoubleArrays.elementSum(data);
 	}
 	
 	@Override
@@ -94,6 +139,16 @@ public final class ImmutableMatrix extends AMatrix {
 	@Override
 	public Matrix toMatrix() {
 		return Matrix.wrap(rows, cols, DoubleArrays.copyOf(data));
+	}
+	
+	@Override
+	public void toDoubleBuffer(DoubleBuffer dest) {
+		dest.put(data);
+	}
+	
+	@Override
+	public Matrix clone() {
+		return Matrix.create(this);
 	}
 
 	@Override
