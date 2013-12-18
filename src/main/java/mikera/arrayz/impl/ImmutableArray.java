69d330ca3055bf27290d7bb773e59f8d1fac7ac2
mikera <mike.r.anderson.13@gmail.com>
2013-12-18 14:43:24 +0000

    Add ImmutableArray

---
+++ src/main/java/mikera/arrayz/impl/ImmutableArray.java
@@ -0,0 +1,116 @@
+package mikera.arrayz.impl;
+
+import mikera.arrayz.INDArray;
+import mikera.vectorz.util.ErrorMessages;
+import mikera.vectorz.util.IntArrays;
+
+public class ImmutableArray extends AbstractArray<INDArray> {
+	private final int dimensions;
+	private final int[] shape;
+	private final int[] strides;
+	private final double[] data;
+
+	private ImmutableArray(int dims, int[] shape, int[] strides) {
+		this.dimensions = dims;
+		this.shape = shape;
+		this.strides = strides;
+		int n = (int) IntArrays.arrayProduct(shape);
+		this.data = new double[n];
+	}
+	
+	private ImmutableArray(int[] shape, double[] data) {
+		this(shape.length, shape, IntArrays.calcStrides(shape), data);
+	}
+
+	private ImmutableArray(int dims, int[] shape, double[] data) {
+		this(dims, shape, IntArrays.calcStrides(shape), data);
+	}
+	
+	public static INDArray wrap(double[] data, int[] shape) {
+		long ec=IntArrays.arrayProduct(shape);
+		if (data.length!=ec) throw new IllegalArgumentException("Data array does not have correct number of elements, expected: "+ec);
+		return new ImmutableArray(shape.length,shape,data);
+	}
+
+	private ImmutableArray(int dims, int[] shape, int[] strides, double[] data) {
+		this.dimensions = dims;
+		this.shape = shape;
+		this.strides = strides;
+		this.data = data;
+	}
+	
+	@Override
+	public int dimensionality() {
+		return dimensions;
+	}
+
+	@Override
+	public int[] getShape() {
+		return shape;
+	}
+	
+	@Override
+	public int[] getShapeClone() {
+		return shape.clone();
+	}
+
+	@Override
+	public long[] getLongShape() {
+		long[] lshape = new long[dimensions];
+		IntArrays.copyIntsToLongs(shape, lshape);
+		return lshape;
+	}
+
+	public int getStride(int dim) {
+		return strides[dim];
+	}
+
+	public int getIndex(int... indexes) {
+		int ix = 0;
+		for (int i = 0; i < dimensions; i++) {
+			ix += indexes[i] * getStride(i);
+		}
+		return ix;
+	}
+
+	@Override
+	public double get(int... indexes) {
+		return data[getIndex(indexes)];
+	}
+
+	@Override
+	public void set(int[] indexes, double value) {
+		throw new UnsupportedOperationException(ErrorMessages.immutable(this));
+	}
+
+	@Override
+	public INDArray slice(int majorSlice) {
+		throw new UnsupportedOperationException("TODO: slices not yet supported on immutable arrays");
+	}
+
+	@Override
+	public INDArray slice(int dimension, int index) {
+		throw new UnsupportedOperationException("TODO: slices not yet supported on immutable arrays");
+	}
+
+	@Override
+	public int sliceCount() {
+		return shape[0];
+	}
+
+	@Override
+	public long elementCount() {
+		return IntArrays.arrayProduct(shape);
+	}
+
+	@Override
+	public boolean isView() {
+		return false;
+	}
+
+	@Override
+	public INDArray exactClone() {
+		return new ImmutableArray(this.shape,data.clone());
+	}
+
+}
