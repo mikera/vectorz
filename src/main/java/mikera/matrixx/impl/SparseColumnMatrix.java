bf127d63223f2b8d867d01456e030fddbafe41b3
mikera <mike.r.anderson.13@gmail.com>
2014-01-02 20:07:50 +0000

    refactoring

--- src/main/java/mikera/matrixx/impl/SparseColumnMatrix.java
+++ src/main/java/mikera/matrixx/impl/SparseColumnMatrix.java
@@ -2,6 +2,7 @@ package mikera.matrixx.impl;
 
 import mikera.arrayz.ISparse;
 import mikera.matrixx.AMatrix;
+import mikera.matrixx.Matrix;
 import mikera.vectorz.AVector;
 import mikera.vectorz.Op;
 import mikera.vectorz.util.ErrorMessages;
@@ -36,6 +37,34 @@ public class SparseColumnMatrix extends AMatrix implements ISparse {
 	public int columnCount() {
 		return columnCount;
 	}
+	
+
+	@Override
+	public boolean isMutable() {
+		for (int i=0; i<columnCount; i++) {
+			AVector v=cols[i];
+			if (v.isMutable()) return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean isFullyMutable() {
+		for (int i=0; i<columnCount; i++) {
+			AVector v=cols[i];
+			if (!v.isFullyMutable()) return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean isZero() {
+		for (int i=0; i<columnCount; i++) {
+			AVector v=cols[i];
+			if (!v.isZero()) return false;
+		}
+		return true;
+	}
 
 	@Override
 	public double get(int row, int column) {
@@ -69,32 +98,6 @@ public class SparseColumnMatrix extends AMatrix implements ISparse {
 		cols[column].set(row,value);
 	}
 
-	@Override
-	public boolean isMutable() {
-		for (int i=0; i<columnCount; i++) {
-			AVector v=cols[i];
-			if (v.isMutable()) return true;
-		}
-		return false;
-	}
-	
-	@Override
-	public boolean isFullyMutable() {
-		for (int i=0; i<columnCount; i++) {
-			AVector v=cols[i];
-			if (!v.isFullyMutable()) return false;
-		}
-		return true;
-	}
-	
-	@Override
-	public boolean isZero() {
-		for (int i=0; i<columnCount; i++) {
-			AVector v=cols[i];
-			if (!v.isZero()) return false;
-		}
-		return true;
-	}
 
 	@Override
 	public SparseColumnMatrix exactClone() {
@@ -124,6 +127,15 @@ public class SparseColumnMatrix extends AMatrix implements ISparse {
 			if (columns[i].length()!=rc) throw new IllegalArgumentException("Mismatched row count at column: "+i);
 		}
 		return new SparseColumnMatrix(columns.clone());
+	}
+	
+	@Override
+	public Matrix toMatrixTranspose() {
+		Matrix m=Matrix.create(columnCount, rowCount);
+		for (int i=0; i<columnCount; i++) {
+			cols[i].getElements(m.data, rowCount*i);
+		}
+		return m;
 	}
 
 }
