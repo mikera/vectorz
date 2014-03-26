a17d1e4854f3bb2930e282145efe24ec9502297a
mikera <mike.r.anderson.13@gmail.com>
2014-03-26 15:53:04 +0800

    remove bad shadowing private field!

--- src/main/java/mikera/vectorz/impl/StridedVector.java
+++ src/main/java/mikera/vectorz/impl/StridedVector.java
@@ -7,7 +7,6 @@ import mikera.vectorz.util.VectorzException;
 public final class StridedVector extends AStridedVector {
 	private static final long serialVersionUID = 5807998427323932401L;
 	
-	private final double[] data;
 	private final int offset;
 	private final int stride;
 	
@@ -19,7 +18,6 @@ public final class StridedVector extends AStridedVector {
 			int lastOffset=(offset+(length-1)*stride);
 			if ((lastOffset>=data.length)||(lastOffset<0)) throw new IndexOutOfBoundsException();
 		}
-		this.data=data;
 		this.offset=offset;
 		this.stride=stride;
 	}
