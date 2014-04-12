49a853b3f8fda10d67da949774f5bad1e4ab76da
mikera <mike.r.anderson.13@gmail.com>
2014-04-12 13:19:51 +0800

    Add determinant code

---
+++ src/main/java/mikera/matrixx/algo/Determinant.java
@@ -0,0 +1,59 @@
+package mikera.matrixx.algo;
+
+import mikera.matrixx.AMatrix;
+import mikera.matrixx.Matrix33;
+import mikera.matrixx.algo.decompose.lu.ILUP;
+import mikera.matrixx.algo.decompose.lu.impl.SimpleLUP;
+import mikera.vectorz.util.IntArrays;
+
+public class Determinant {
+
+	public static double calc(AMatrix m) {
+		int rc = m.rowCount();
+		int cc = m.columnCount();
+		if (rc!=cc)
+			throw new UnsupportedOperationException(
+					"Cannot take determinant of non-square matrix!");
+
+		if (rc==1) return m.unsafeGet(0,0);
+		if (rc==2) return m.unsafeGet(0,0)*m.unsafeGet(1,1)-m.unsafeGet(1,0)*m.unsafeGet(0,1);
+		if (rc==3) {
+			return new Matrix33(m).determinant();
+		}
+		
+		return smartDeterminant(m);		
+	}
+	
+	static double smartDeterminant(AMatrix m) {
+		ILUP lup=SimpleLUP.decompose(m);
+		double det=lup.getL().diagonalProduct()*lup.getU().diagonalProduct()*lup.getP().determinant();
+		return det;
+	}
+	
+	@SuppressWarnings("unused")
+	static double naiveDeterminant(AMatrix m) {
+		int rc = m.rowCount();
+		int[] inds = new int[rc];
+		for (int i = 0; i < rc; i++) {
+			inds[i] = i;
+		}
+		return calcDeterminant(m,inds, 0);
+	}
+
+	private static double calcDeterminant(AMatrix m, int[] inds, int offset) {
+		int rc = m.rowCount();
+		if (offset == (rc - 1))
+			return m.unsafeGet(offset, inds[offset]);
+
+		double det = m.unsafeGet(offset, inds[offset])
+				* calcDeterminant(m,inds, offset + 1);
+		for (int i = 1; i < (rc - offset); i++) {
+			IntArrays.swap(inds, offset, offset + i);
+			det -= m.unsafeGet(offset, inds[offset])
+					* calcDeterminant(m,inds, offset + 1);
+			IntArrays.swap(inds, offset, offset + i);
+		}
+		return det;
+	}
+
+}
