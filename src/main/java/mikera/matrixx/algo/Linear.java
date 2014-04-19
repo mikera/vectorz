b4084d68182c9140d098f844f621296306de28ed
mikera <mike.r.anderson.13@gmail.com>
2014-04-19 13:34:44 +0800

    docs

--- src/main/java/mikera/matrixx/algo/Linear.java
+++ src/main/java/mikera/matrixx/algo/Linear.java
@@ -1,10 +1,14 @@
 package mikera.matrixx.algo;
 
+import mikera.indexz.Index;
 import mikera.matrixx.AMatrix;
 import mikera.vectorz.AVector;
 
 /**
- * Class for liner solver algorithms
+ * Class providing liner solver algorithms that find the solution to systems of the form:
+ * 
+ *    A.x = b
+ * 
  * @author Mike
  *
  */
@@ -25,7 +29,7 @@ public class Linear {
 	}
 	
 	public static AVector solveSquare(AMatrix a, AVector b) {
-		if (!a.isSquare()) throw new IllegalArgumentException("matrix must be square");
+		if (!a.isSquare()) throw new IllegalArgumentException("Matrix must be square but got shape: "+Index.of(a.getShape()));
 		AMatrix m=a.inverse();
 		if (m==null) return null;
 		return m.transform(b);
