eb39d9d43643af6a49f9ccc88eaff339d65db06a
mikera <mike.r.anderson.13@gmail.com>
2014-04-20 15:47:51 +0800

    more Cholesky docs

--- src/main/java/mikera/matrixx/algo/Cholesky.java
+++ src/main/java/mikera/matrixx/algo/Cholesky.java
@@ -6,11 +6,26 @@ import mikera.util.Maths;
 
 /**
  * Class implementing a standard Cholesky decomposition
+ * 
+ *    A = L.L*
+ *    
+ * Where: A  is a symmetric (actually Hermitian), positive-definite matrix 
+ * and:   L  is a lower triangular matrix
+ *        L* is the conjugate transpose of L (which is equal to its transpose, since A is real in Vectorz)
+ * 
+ * See: http://en.wikipedia.org/wiki/Cholesky_decomposition
+ * 
  * @author Mike
  *
  */
 public class Cholesky {
 
+	/**
+	 * Decompose a matrix according the the Cholesky decomposition A = L.L*
+	 * 
+	 * @param a Any symmetric, positive definite matrix
+	 * @return The lower triangular decomposition result L
+	 */
 	public static final Matrix decompose(AMatrix a) {
 		return decompose(Matrix.create(a));
 	}
