d4640ae1d8e66c5d7174eb9b3ab6d070df67bc65
mikera <mike.r.anderson.13@gmail.com>
2014-04-20 18:07:04 +0800

    IFastRows and IFastColumns should be O(1) time and space

--- src/main/java/mikera/matrixx/impl/IFastColumns.java
+++ src/main/java/mikera/matrixx/impl/IFastColumns.java
@@ -7,7 +7,7 @@ import mikera.vectorz.AVector;
  * Marker interface for matrices with fast column access
  * 
  * General intention is that any matrix that implements this must have a fast getColumn(), typically
- * - At most one small object allocation
+ * - O(1) time and space
  * - An efficient vector type returned
  * 
  * @author Mike
--- src/main/java/mikera/matrixx/impl/IFastRows.java
+++ src/main/java/mikera/matrixx/impl/IFastRows.java
@@ -7,7 +7,7 @@ import mikera.vectorz.AVector;
  * Marker interface for matrices with fast row access
  * 
  * General intention is that any matrix that implements this must have a fast getRow(), typically
- * - At most one small object allocation
+ * - O(1) time and space
  * - An efficient vector type returned (a strided vector or better)
  * 
  * @author Mike
