bbd400b0f3d0915c7383cf3c0aac9ab82a704efa
mikera <mike.r.anderson.13@gmail.com>
2014-05-23 17:22:21 +0100

    more optimisation

--- src/main/java/mikera/matrixx/impl/ADiagonalMatrix.java
+++ src/main/java/mikera/matrixx/impl/ADiagonalMatrix.java
@@ -211,13 +211,10 @@ public abstract class ADiagonalMatrix extends ASingleBandMatrix {
 	@Override
 	public Matrix innerProduct(Matrix a) {
 		if (!(dimensions==a.rowCount())) throw new IllegalArgumentException(ErrorMessages.incompatibleShapes(this,a));
-		int acc=a.columnCount();
-		Matrix m=Matrix.create(dimensions, acc);
+		Matrix m=a.clone();
 		for (int i=0; i<dimensions; i++) {
 			double dv=unsafeGetDiagonalValue(i);
-			for (int j=0; j<acc; j++) {
-				m.unsafeSet(i, j, dv*a.unsafeGet(i,j));
-			}
+			m.multiplyRow(i, dv);
 		}
 		return m;
 	}
