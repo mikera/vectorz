c680b617cde390beb25d0365c5d537ffbc70c1a1
mikera <mike.r.anderson.13@gmail.com>
2014-05-23 13:03:09 +0100

    Create bidiagonal API class

---
+++ src/main/java/miker/matrixx/decompose/Bidiagonal.java
@@ -0,0 +1,25 @@
+package miker.matrixx.decompose;
+
+import mikera.matrixx.AMatrix;
+import mikera.matrixx.decompose.impl.bidiagonal.BidiagonalRow;
+
+/**
+ * API class for performing bidiagonal decompositions
+ * 
+ * @author Mike
+ *
+ */
+public class Bidiagonal {
+
+	public static IBidiagonalResult decompose(AMatrix A) {
+		return BidiagonalRow.decompose(A);
+	}
+	
+	public static IBidiagonalResult decompose(AMatrix A, boolean compact) {
+		return BidiagonalRow.decompose(A,compact);
+	}
+	
+	public static IBidiagonalResult decomposeCompact(AMatrix A) {
+		return decompose(A,true);
+	}
+}
