0b1d913891e25d3c3570ce7a3c400cba224b809c
mikera <mike.r.anderson.13@gmail.com>
2014-05-31 12:55:30 +0100

    refactor determinant code

--- src/main/java/mikera/matrixx/algo/Determinant.java
+++ src/main/java/mikera/matrixx/algo/Determinant.java
@@ -32,19 +32,34 @@ public class Determinant {
 			throw new UnsupportedOperationException(ErrorMessages.nonSquareMatrix(m));
 		}
 
+		if (rc<=3) return calculateSmallDeterminant(m,rc);
+		
+		return calculateLUPDeterminant(m);		
+	}
+	
+	/**
+	 * Determinant implemented using the LUP decomposition
+	 * 
+	 * @param m
+	 * @return
+	 */
+	static double calculateLUPDeterminant(AMatrix m) {
+		ILUPResult lup=SimpleLUP.decompose(m);
+		double det=lup.getL().diagonalProduct()*lup.getU().diagonalProduct()*lup.getP().determinant();
+		return det;
+	}
+	
+	/**
+	 * Calculates the determinant of a small square matrix via direct computation
+	 * @return
+	 */
+	static double calculateSmallDeterminant(AMatrix m, int rc) {
 		if (rc==1) return m.unsafeGet(0,0);
 		if (rc==2) return m.unsafeGet(0,0)*m.unsafeGet(1,1)-m.unsafeGet(1,0)*m.unsafeGet(0,1);
 		if (rc==3) {
 			return new Matrix33(m).determinant();
 		}
-		
-		return smartDeterminant(m);		
-	}
-	
-	static double smartDeterminant(AMatrix m) {
-		ILUPResult lup=SimpleLUP.decompose(m);
-		double det=lup.getL().diagonalProduct()*lup.getU().diagonalProduct()*lup.getP().determinant();
-		return det;
+		throw new UnsupportedOperationException("Small determinant calculation on size "+rc+" not possible");
 	}
 	
 	@SuppressWarnings("unused")
