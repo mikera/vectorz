1badd2973462336376a4e5b30a4cc1709854e248
Prasant Chidella <prasant12075@iiitd.ac.in>
2014-06-20 21:50:38 +0530

    change api to use SVD instead of SvdImplicitQr, add method to compute rank from ISVDResult

--- src/main/java/mikera/matrixx/algo/Rank.java
+++ src/main/java/mikera/matrixx/algo/Rank.java
@@ -1,35 +1,62 @@
 package mikera.matrixx.algo;
 
 import mikera.matrixx.AMatrix;
-import mikera.matrixx.decompose.impl.svd.SVDResult;
-import mikera.matrixx.decompose.impl.svd.SvdImplicitQr;
+import mikera.matrixx.decompose.ISVDResult;
+import mikera.matrixx.decompose.SVD;
 import mikera.vectorz.AVector;
 
 public class Rank {
 	
-	private static double threshold = 2.220446e-15;
+	private static double DEFAULT_THRESHOLD = 2.220446e-15;
 	
 	/**
-     * Returns the rank of a matrix.
-     * 
-     * @param A The input matrix
-     * @return The matrix's rank
-     */
+	 * Returns the rank of a matrix.
+	 * 
+	 * @param A The input matrix
+	 * @return The matrix's rank
+	 */
 	public static int compute(AMatrix A) {
-		return compute(A, threshold);
+		return compute(A, DEFAULT_THRESHOLD);
 	}
 	
 	/**
-     * Returns the rank of a matrix.
+	 * Returns the rank of a matrix.
+	 * 
+	 * @param A The input matrix
+	 * @param threshold Tolerance used to determine if a singular value is singular.
+	 * @return The matrix's rank
+	 */
+	public static int compute(AMatrix A, double threshold) {
+		ISVDResult ans = SVD.decompose(A, true);
+		int rank = 0;
+		AVector singularValues = ans.getSingularValues();
+		for( double s : singularValues ) {
+			if( s > threshold)
+				rank++;
+		}
+		return rank;
+	}
+	
+	/**
+     * Directly computes rank of matrix whose SVD decomposition has already been computed.
      * 
-     * @param A The input matrix
+     * @param result The result of an SVD decomposition
+     * @return The matrix's rank
+     */
+	public static int compute(ISVDResult result) {
+		return compute(result, DEFAULT_THRESHOLD);
+	}
+	
+	/**
+     * Directly computes rank of matrix whose SVD decomposition has already been computed.
+     * 
+     * @param result The result of an SVD decomposition
      * @param threshold Tolerance used to determine if a singular value is singular.
      * @return The matrix's rank
      */
-	public static int compute(AMatrix A, double threshold) {
-		SVDResult ans = SvdImplicitQr.decompose(A, true);
+	public static int compute(ISVDResult result, double threshold) {
 		int rank = 0;
-		AVector singularValues = ans.getSingularValues();
+		AVector singularValues = result.getSingularValues();
         for( double s : singularValues ) {
             if( s > threshold)
                 rank++;
