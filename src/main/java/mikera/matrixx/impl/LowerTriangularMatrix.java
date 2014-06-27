b9a27ba61d8aaea2b015e2e918800bb76087359f
mikera <mike.r.anderson.13@gmail.com>
2014-06-27 12:15:03 +0100

    tidy

--- src/main/java/mikera/matrixx/impl/LowerTriangularMatrix.java
+++ src/main/java/mikera/matrixx/impl/LowerTriangularMatrix.java
@@ -7,7 +7,6 @@ import mikera.vectorz.AVector;
 import mikera.vectorz.Vectorz;
 import mikera.vectorz.impl.ArraySubVector;
 import mikera.vectorz.impl.IndexedArrayVector;
-import mikera.vectorz.util.ErrorMessages;
 
 /**
  * Class for a lower triangular matrix packed densely by rows.
@@ -86,7 +85,7 @@ public final class LowerTriangularMatrix extends ATriangularMatrix implements IF
 
 	@Override
 	public double get(int i, int j) {
-		if ((i<0)||(i>=rows)||(j<0)||(j>=cols)) throw new IndexOutOfBoundsException(ErrorMessages.invalidIndex(this, i,j));
+		checkIndex(i,j);
 		if (j>i) return 0.0;
 		return data[internalIndex(i,j)];
 	}
@@ -122,11 +121,11 @@ public final class LowerTriangularMatrix extends ATriangularMatrix implements IF
 	
 	@Override
 	public boolean equals(AMatrix a) {
-		if (a==this) return true;	
 		if (a instanceof ADenseArrayMatrix) {
 			return equals((ADenseArrayMatrix)a);
 		}
 
+		if (a==this) return true;	
 		if (!isSameShape(a)) return false;
 		
 		for (int i = 0; i < rows; i++) {
