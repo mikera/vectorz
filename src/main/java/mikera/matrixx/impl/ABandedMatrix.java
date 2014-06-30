5902e41b970a78bc920875e287996ed55d107351
mikera <mike.r.anderson.13@gmail.com>
2014-06-30 21:10:22 +0100

    banded matrix refactoring

--- src/main/java/mikera/matrixx/impl/ABandedMatrix.java
+++ src/main/java/mikera/matrixx/impl/ABandedMatrix.java
@@ -172,7 +172,7 @@ public abstract class ABandedMatrix extends AMatrix implements ISparse, IFastBan
 
 		@Override
 		public double get(int i) {
-			if ((i<0)||(i>=length)) throw new IndexOutOfBoundsException("Index: "+i);
+			checkIndex(i);
 			return unsafeGet(i);
 		}
 		
@@ -203,7 +203,7 @@ public abstract class ABandedMatrix extends AMatrix implements ISparse, IFastBan
 
 		@Override
 		public void set(int i, double value) {
-			if ((i<0)||(i>=length)) throw new IndexOutOfBoundsException("Index: "+i);
+			checkIndex(i);
 			unsafeSet(i,value);
 		}
 		
@@ -234,6 +234,14 @@ public abstract class ABandedMatrix extends AMatrix implements ISparse, IFastBan
 			int di = offset+this.bandStartColumn(b)+cc*bandStartRow(b);
 			band.addToArray(data, di, cc+1);
 		}
+	}
+	
+	@Override
+	public double[] toDoubleArray() {
+		double[] result=Matrix.createStorage(rowCount(),columnCount());
+		// since this array is sparse, fastest to use addToArray to modify only non-zero elements
+		addToArray(result,0);
+		return result;
 	}
 	
 	@Override public void validate() {
