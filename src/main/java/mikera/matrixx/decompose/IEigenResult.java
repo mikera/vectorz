256d518baf4a9143cc1fd56943cb7311cb90eb9d
Prasant Chidella <prasant12075@iiitd.ac.in>
2014-08-05 20:13:25 +0530

    documentation for Eigen and IEigenResult

--- src/main/java/mikera/matrixx/decompose/IEigenResult.java
+++ src/main/java/mikera/matrixx/decompose/IEigenResult.java
@@ -6,14 +6,21 @@ import mikera.vectorz.Vector2;
 public interface IEigenResult {
     
     /**
+     * <p>
+     * Returns an array of eigenvalue as complex numbers.  For symmetric matrices the returned eigenvalues will always be real
+     * numbers, which means the imaginary components will be equal to zero.
+     * </p>
      * 
-     * @return
+     * @return An array of eigenvalues.
      */
     public Vector2[] getEigenvalues();
     
     /**
-     * 
-     * @return
+     * <p>
+     * Used to retrieve an array real valued eigenvectors.
+     * </p>
+     *
+     * @return If the associated eigenvalue is real then an eigenvector is returned, null otherwise.
      */
     public AVector[] getEigenVectors();
 }
