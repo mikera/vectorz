70458b68a5398695c87b57a8b39552f09eb71d04
mikera <mike.r.anderson.13@gmail.com>
2015-01-11 12:45:54 +0800

    delete dead code

--- src/main/java/mikera/vectorz/util/Errors.java
+++
@@ -1,9 +0,0 @@
-package mikera.vectorz.util;
-
-import mikera.arrayz.INDArray;
-
-public class Errors {
-	public static Throwable immutable(INDArray a) {
-		return new UnsupportedOperationException(ErrorMessages.immutable(a));
-	}
-}
