b7cc5df49dac970e67bddc09cc77c29f2ace7268
mikera <mike.r.anderson.13@gmail.com>
2015-05-20 09:23:10 +0800

    Extra test for matrix by vector divide (auto-broadcast case)

--- src/test/java/mikera/matrixx/impl/TestMiscMatrices.java
+++ src/test/java/mikera/matrixx/impl/TestMiscMatrices.java
@@ -1,6 +1,8 @@
 package mikera.matrixx.impl;
 
 import mikera.indexz.Index;
+import mikera.matrixx.Matrix;
+import mikera.vectorz.Vector;
 
 import org.junit.Test;
 
@@ -13,5 +15,11 @@ public class TestMiscMatrices {
 
 	@Test public void testSubsetIdentity() {
 		assertTrue(SubsetMatrix.create(Index.of(0,1,2),3).isIdentity());
+	}
+	
+	@Test public void testDivideByVector() {
+		Matrix m=Matrix.create(new double[][]{{1,2},{3,4}});
+		m.divide(Vector.of(1,2));
+		assertEquals(Matrix.create(new double[][]{{1,1},{3,2}}),m);
 	}
 }
