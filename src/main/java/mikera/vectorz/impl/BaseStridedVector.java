3241448c846e988bc2aca7817c567de498ca6171
mikera <mike.r.anderson.13@gmail.com>
2015-07-30 13:03:37 +0800

    more refactoring of code into BaseStridedVector

--- src/main/java/mikera/vectorz/impl/BaseStridedVector.java
+++ src/main/java/mikera/vectorz/impl/BaseStridedVector.java
@@ -1,13 +1,25 @@
 package mikera.vectorz.impl;
 
 import mikera.vectorz.AVector;
+import mikera.vectorz.util.VectorzException;
 
+/**
+ * Base class for strided vectors backed with a double array and fixed offset / stride
+ * @author Mike
+ *
+ */
 public abstract class BaseStridedVector extends AStridedVector {
 
 	protected BaseStridedVector(int length, double[] data, int offset, int stride) {
 		super(length, data);
 		this.stride=stride;
 		this.offset=offset;
+		if ((offset<0)) throw new IndexOutOfBoundsException();
+		if (length>0) {
+			// check last element is in the array
+			int lastOffset=(offset+(length-1)*stride);
+			if ((lastOffset>=data.length)||(lastOffset<0)) throw new IndexOutOfBoundsException("StridedVector ends outside array");
+		}
 	}
 
 	protected final int stride;
@@ -24,7 +36,7 @@ public abstract class BaseStridedVector extends AStridedVector {
 	}
 
 	@Override
-	protected int index(int i) {
+	protected final int index(int i) {
 		return offset+i*stride;
 	}
 
@@ -68,5 +80,57 @@ public abstract class BaseStridedVector extends AStridedVector {
 	public void set(AVector v) {
 		int length=checkSameLength(v);
 		v.copyTo(0, data, offset, length, stride);
+	}
+	
+	
+	@Override
+	public void add(AVector v) {
+		if (v instanceof AStridedVector) {
+			add((AStridedVector)v);
+			return;
+		}
+		super.add(v);
+	}
+	
+	public void add(AStridedVector v) {
+		int length=checkLength(v.length());
+		double[] vdata=v.getArray();
+		int voffset=v.getArrayOffset();
+		int vstride=v.getStride();
+		for (int i=0; i<length; i++) {
+			data[offset+i*stride]+=vdata[voffset+i*vstride];
+		}
+	}
+	
+	@Override
+	public double dotProduct(AVector v) {
+		int length=checkLength(v.length());
+		if (v instanceof ADenseArrayVector) {
+			ADenseArrayVector av=(ADenseArrayVector) v;
+			return dotProduct(av.getArray(),av.getArrayOffset());
+		}
+		double result=0.0;
+		for (int i=0; i<length; i++) {
+			result+=data[offset+i*stride]*v.unsafeGet(i);
+		}
+		return result;
+	}
+	
+	@Override
+	public void getElements(double[] dest, int destOffset) {
+		for (int i=0; i<length; i++) {
+			dest[destOffset+i]=data[offset+(i*stride)];
+		}
+	}
+	
+	@Override
+	public void validate() {
+		if (length>0) {
+			if ((offset<0)||(offset>=data.length)) throw new VectorzException("offset out of bounds: "+offset);
+			int lastIndex=offset+(stride*(length-1));
+			if ((lastIndex<0)||(lastIndex>=data.length)) throw new VectorzException("lastIndex out of bounds: "+lastIndex);
+		}
+		
+		super.validate();
 	}
 }
--- src/main/java/mikera/vectorz/impl/StridedVector.java
+++ src/main/java/mikera/vectorz/impl/StridedVector.java
@@ -8,12 +8,6 @@ public final class StridedVector extends BaseStridedVector {
 	
 	private StridedVector(double[] data, int offset, int length, int stride) {
 		super(length,data,offset,stride);
-		if ((offset<0)) throw new IndexOutOfBoundsException();
-		if (length>0) {
-			// check last element is in the array
-			int lastOffset=(offset+(length-1)*stride);
-			if ((lastOffset>=data.length)||(lastOffset<0)) throw new IndexOutOfBoundsException("StridedVector ends outside array");
-		}
 	}
 	
 	public static StridedVector wrapStrided(double[] data, int offset, int length, int stride) {
@@ -40,39 +34,6 @@ public final class StridedVector extends BaseStridedVector {
 	}
 	
 	@Override
-	public double dotProduct(AVector v) {
-		int length=checkLength(v.length());
-		if (v instanceof ADenseArrayVector) {
-			ADenseArrayVector av=(ADenseArrayVector) v;
-			return dotProduct(av.getArray(),av.getArrayOffset());
-		}
-		double result=0.0;
-		for (int i=0; i<length; i++) {
-			result+=data[offset+i*stride]*v.unsafeGet(i);
-		}
-		return result;
-	}
-	
-	@Override
-	public void add(AVector v) {
-		if (v instanceof AStridedVector) {
-			add((AStridedVector)v);
-			return;
-		}
-		super.add(v);
-	}
-	
-	public void add(AStridedVector v) {
-		int length=checkLength(v.length());
-		double[] vdata=v.getArray();
-		int voffset=v.getArrayOffset();
-		int vstride=v.getStride();
-		for (int i=0; i<length; i++) {
-			data[offset+i*stride]+=vdata[voffset+i*vstride];
-		}
-	}
-	
-	@Override
 	public AVector subVector(int start, int length) {
 		int len=checkRange(start,length);
 
@@ -86,31 +47,8 @@ public final class StridedVector extends BaseStridedVector {
 	}
 	
 	@Override
-	public void getElements(double[] dest, int destOffset) {
-		for (int i=0; i<length; i++) {
-			dest[destOffset+i]=data[offset+(i*stride)];
-		}
-	}
-	
-	@Override
 	public StridedVector exactClone() {
 		double[] data=this.data.clone();
 		return wrapStrided(data,offset,length,stride);
-	}
-
-	@Override
-	public void validate() {
-		if (length>0) {
-			if ((offset<0)||(offset>=data.length)) throw new VectorzException("offset out of bounds: "+offset);
-			int lastIndex=offset+(stride*(length-1));
-			if ((lastIndex<0)||(lastIndex>=data.length)) throw new VectorzException("lastIndex out of bounds: "+lastIndex);
-		}
-		
-		super.validate();
-	}
-
-	@Override
-	protected int index(int i) {
-		return offset+i*stride;
 	}
 }
