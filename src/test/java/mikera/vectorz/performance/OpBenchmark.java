c56a0932fbfa8acc31ac568abf59279f69dc17bd
Mike <mike@mikera.net>
2015-12-19 12:55:04 +0000

    More OpBenchmark tweaks

--- src/test/java/mikera/vectorz/performance/OpBenchmark.java
+++ src/test/java/mikera/vectorz/performance/OpBenchmark.java
@@ -3,12 +3,12 @@ package mikera.vectorz.performance;
 import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 
+import mikera.vectorz.AVector;
 import mikera.vectorz.Op2;
 import mikera.vectorz.Ops;
 import mikera.vectorz.Vector;
 import mikera.vectorz.Vectorz;
 import mikera.vectorz.ops.AddFunction;
-import mikera.vectorz.ops.Power;
 
 /**
  * Caliper based benchmarks
@@ -17,10 +17,10 @@ import mikera.vectorz.ops.Power;
  */
 
 public class OpBenchmark extends SimpleBenchmark {
-	private static final int VECTOR_SIZE = 1000;
-	private static final Vector a=Vector.createLength(VECTOR_SIZE);
-	private static final Vector b=Vector.createLength(VECTOR_SIZE);
-	private static final Op2 op=AddFunction.create(0.5,0.5,Ops.SQUARE);
+	public static final int VECTOR_SIZE = 1000;
+	public static AVector a=Vector.createLength(VECTOR_SIZE);
+	public static AVector b=Vector.createLength(VECTOR_SIZE);
+	public static Op2 op=AddFunction.create(0.5,0.5,Ops.SQUARE);
 
 	static {
 		Vectorz.fillGaussian(a);
@@ -32,7 +32,7 @@ public class OpBenchmark extends SimpleBenchmark {
 	// op benchmark for z = 0.5*x + 0.5*y*y
 	
 	public void timeOp(int runs) {
-		Vector t=Vector.createLength(VECTOR_SIZE);
+		AVector t=Vector.createLength(VECTOR_SIZE);
 		for (int run=0; run<runs; run++) {
 			t.set(a);
 			t.applyOp(op, b);
@@ -40,8 +40,8 @@ public class OpBenchmark extends SimpleBenchmark {
 		}
 	}
 	
-	public void timeManual(int runs) {
-		Vector t=Vector.createLength(VECTOR_SIZE);
+	public void timeFunctions(int runs) {
+		AVector t=Vector.createLength(VECTOR_SIZE);
 		for (int run=0; run<runs; run++) {
 			t.set(b);
 			t.square();
@@ -52,7 +52,7 @@ public class OpBenchmark extends SimpleBenchmark {
 	}
 	
 	public void timeElementwise(int runs) {
-		Vector t=Vector.createLength(VECTOR_SIZE);
+		AVector t=Vector.createLength(VECTOR_SIZE);
 		for (int run=0; run<runs; run++) {
 			for (int j=0; j<VECTOR_SIZE; j++) {
 				double x=a.unsafeGet(j);
