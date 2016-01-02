c3dc9bb44c5ac3162e02eee9f28b5a2805955f1e
Mike <mike@mikera.net>
2016-01-02 11:39:28 +0000

    Make IScalar extend INDArray

--- src/main/java/mikera/vectorz/IScalar.java
+++ src/main/java/mikera/vectorz/IScalar.java
@@ -1,5 +1,6 @@
 package mikera.vectorz;
 
+import mikera.arrayz.INDArray;
 import mikera.vectorz.impl.ImmutableScalar;
 
 /**
@@ -7,26 +8,30 @@ import mikera.vectorz.impl.ImmutableScalar;
  * 
  * @author Mike
  */
-public interface IScalar {
+public interface IScalar extends INDArray {
 
 	/**
 	 * Returns the double value of a scalar array
 	 * @return
 	 */
+	@Override
 	public double get();
 	
 	/**
 	 * Sets the double value of a scalar array
 	 */
+	@Override
 	public void set(double value);
 	
 	/**
 	 * Coerces this scalar to a mutable form
 	 */
+	@Override
 	public AScalar mutable();
 	
 	/**
 	 * Coerces this scalar to an immutable form
 	 */
+	@Override
 	public ImmutableScalar immutable();
 }
