89ce364cb870747d43728b92da62a90649c7367c
mikera <mike.r.anderson.13@gmail.com>
2017-08-03 11:16:15 +0800

    Add benchmark for different array types

---
+++ src/test/java/mikera/vectorz/performance/VectorzTypeBenchmark.java
@@ -0,0 +1,81 @@
+package mikera.vectorz.performance;
+
+import com.google.caliper.Runner;
+import com.google.caliper.SimpleBenchmark;
+
+import mikera.arrayz.Array;
+import mikera.arrayz.NDArray;
+import mikera.matrixx.Matrix;
+import mikera.matrixx.impl.StridedMatrix;
+import mikera.vectorz.GrowableVector;
+import mikera.vectorz.Vector;
+import mikera.vectorz.Vectorz;
+
+/**
+ * Caliper based benchmarks
+ * 
+ * @author Mike
+ */
+
+public class VectorzTypeBenchmark extends SimpleBenchmark {
+	private static final int ARRAY_SIZE = 100;
+	private static final int INNER_ITERATIONS = 10;
+	
+	public void timeVector(int runs) {
+		for (int i=0; i<runs; i++) {
+			Vector a=Vector.createLength(ARRAY_SIZE);
+			for (int ii=0; ii<INNER_ITERATIONS; ii++) {
+				a.add(ii);
+			}
+		}
+	}
+	
+	public void timeMatrix(int runs) {
+		for (int i=0; i<runs; i++) {
+			Matrix a=Matrix.create(1,ARRAY_SIZE);
+			for (int ii=0; ii<INNER_ITERATIONS; ii++) {
+				a.add(ii);
+			}
+		}
+	}
+	
+	public void timeStridedMatrix(int runs) {
+		for (int i=0; i<runs; i++) {
+			StridedMatrix a=StridedMatrix.create(1,ARRAY_SIZE);
+			for (int ii=0; ii<INNER_ITERATIONS; ii++) {
+				a.add(ii);
+			}
+		}
+	}
+	
+	public void timeArray(int runs) {
+		for (int i=0; i<runs; i++) {
+			Array a=Array.newArray(1,ARRAY_SIZE);
+			for (int ii=0; ii<INNER_ITERATIONS; ii++) {
+				a.add(ii);
+			}
+		}
+	}
+	
+	public void timeNDArray(int runs) {
+		for (int i=0; i<runs; i++) {
+			NDArray a=NDArray.newArray(1,ARRAY_SIZE);
+			for (int ii=0; ii<INNER_ITERATIONS; ii++) {
+				a.add(ii);
+			}
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new VectorzTypeBenchmark().run();
+	}
+
+	private void run() {
+		Runner runner=new Runner();
+		runner.run(new String[] {this.getClass().getCanonicalName()});
+	}
+
+}
