1d4652db2701c6a46c85834dd8dfba63b5998ab9
Farah <hariri2@illinois.edu>
2017-10-14 13:58:36 -0500

    added a test for testing the function mul from the class Quaternions for boundary values. The overflow results in NaN and Inf

--- src/test/java/mikera/vectorz/TestQuaternions.java
+++ src/test/java/mikera/vectorz/TestQuaternions.java
@@ -21,6 +21,15 @@ public class TestQuaternions {
 		// assertEquals(Vector4.of(0,0,0,1),r2);
 		assertTrue(r2.epsilonEquals(Vector4.of(0,0,0,1)));
 	}
+
+	@Test public void testMulWithBoundaryVals() {
+	    Vector4 a=Vector4.of(Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE);
+	    Vector4 b = new Vector4(Double.MAX_VALUE,0,Double.MAX_VALUE,0);
+	    Vector4 q=Quaternions.mul(a,b);
+	    Vector4 expected=Vector4.of(Double.POSITIVE_INFINITY,Double.NaN,Double.NaN,Double.NEGATIVE_INFINITY);
+	    assertTrue(q.epsilonEquals(expected));
+	}
+
 	
 	@Test public void testRotate() {
 		Vector3 axis=new Vector3(1,0,0);
